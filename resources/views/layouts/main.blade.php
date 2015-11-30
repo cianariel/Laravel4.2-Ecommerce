6a39641a233e78e60c9f87d0326d240beb1ed088
Alex Velikanov <biz@alexvelikanov.com>
2015-11-30 22:57:36 +0700

    [FRONTEND] - beta main header

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,41 +4,50 @@
         <title>Ideaing</title>
 
 {{--        {{ HTML::style('css/app.css') }}--}}
+        <link rel="stylesheet" href="/assets/css/app.css">
     </head>
     <body>
         <header class="colophon">
-            <div class="col-xs-12">
-                <h2>Ideaing | Ideas for Smarter Living</h2>
-                <nav id="top-nav">
-                    <ul>
-                        <li><a href="">Shop</a></li>
-                        <li><a href="">Pros</a></li>
-                        <li><a href="">Discuss</a></li>
-                    </ul>
-                </nav>
-                <img class="logo top-logo"/>
-                <section class="search-bar">
-                    <input type="text" name="search" value="Search..."/>
-                </section>
-                <div>
-                    <a href="#">Sign in</a>
+                <div class="col-xs-12">
+                    <h2 id="site-name">Ideaing | Ideas for Smarter Living</h2>
+                    <nav id="top-nav" class="row">
+                        <div class="container">
+                            <div  class="col-sm-5">
+                                <ul>
+                                    <li class="nested"><a href="">Shop</a></li>
+                                    <li><a href="">Pros</a></li>
+                                    <li><a href="">Discuss</a></li>
+                                </ul>
+                            </div>
+
+                            <div class="col-sm-2">
+                                <img src="/assets/images/ideaing-logo.png" id="ideaing-logo" class="logo top-logo"/>
+                            </div>
+                            <section class="search-bar col-sm-2">
+                                <input class="form-control" type="text" name="search" value="Search..."/>
+                            </section>
+                            <div class="col-sm-2 pull-right signin">
+                                <a href="#">Sign in</a>
+                            </div>
+                        </div>
+                    </nav>
                 </div>
-            </div>
 
-            <nav id="mid-nav">
-                <ul>
-                    <li><a href="">All ideas</a></li>
-                    <li><a href="">Kitchen</a></li>
-                    <li><a href="">Bedroom</a></li>
-                    <li><a href="">Office</a></li>
-                    <li><a href="">Living</a></li>
-                    <li><a href="">Outdoor</a></li>
-                    <li><a href="">Lighting</a></li>
-                    <li><a href=""></a>Decor</li>
-                    <li><a href=""></a>...</li>
-                </ul>
-            </nav>
-
+                <nav id="mid-nav"  class="col-xs-12">
+                    <div class="container">
+                        <ul>
+                            <li class="selected"><a href="">All ideas</a></li>
+                            <li><a href="">Kitchen</a></li>
+                            <li><a href="">Bedroom</a></li>
+                            <li><a href="">Office</a></li>
+                            <li><a href="">Living</a></li>
+                            <li><a href="">Outdoor</a></li>
+                            <li><a href="">Lighting</a></li>
+                            <li><a href="">Decor</a></li>
+                            <li><a href="">...</a></li>
+                        </ul>
+                    </div>
+                </nav>
         </header>
 
 
