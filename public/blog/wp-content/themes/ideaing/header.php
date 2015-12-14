ffe8f6c2995b3dafdcc04734f5699d7cf0122948
Alex Velikanov <biz@alexvelikanov.com>
2015-12-14 18:14:24 +0700

    ++

--- public/blog/wp-content/themes/ideaing/header.php
+++ public/blog/wp-content/themes/ideaing/header.php
@@ -1,12 +1,11 @@
 <!doctype html>
 <html <?php language_attributes(); ?> class="no-js idea-stories">
 	<head>
-		<link href="//www.google-analytics.com" rel="dns-prefetch">
-<!--        <link href="--><?php //echo get_template_directory_uri(); ?><!--/img/icons/favicon.ico" rel="shortcut icon">-->
-<!--        <link href="--><?php //echo get_template_directory_uri(); ?><!--/img/icons/touch.png" rel="apple-touch-icon-precomposed">-->
 
+		<?php wp_head(); ?>
 
         <?php loadLaravelView('head'); ?>
+
 
 	</head>
 	<body <?php body_class(); ?>>
