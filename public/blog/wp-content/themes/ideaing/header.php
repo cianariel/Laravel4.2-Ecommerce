92eb39afa9043b5aec91e4b5f70a89afe3d2126b
Alex Velikanov <biz@alexvelikanov.com>
2015-12-14 15:46:54 +0700

    Disable WP stylesheets and scripts

--- public/blog/wp-content/themes/ideaing/header.php
+++ public/blog/wp-content/themes/ideaing/header.php
@@ -5,23 +5,30 @@
 		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
 
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
-        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
-        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+<!--        <link href="--><?php //echo get_template_directory_uri(); ?><!--/img/icons/favicon.ico" rel="shortcut icon">-->
+<!--        <link href="--><?php //echo get_template_directory_uri(); ?><!--/img/icons/touch.png" rel="apple-touch-icon-precomposed">-->
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script>
+            if (screen.width < 992 && screen.width > 620) {
+                document.getElementById("viewport").setAttribute("content", "width=980");
+            }
+        </script>
 
 		<meta name="description" content="<?php bloginfo('description'); ?>">
 
 		<?php wp_head(); ?>
-		<script>
+
+        <link rel="stylesheet" href="/assets/css/app.css">
+
+        <script>
         // conditionizr.com
         // configure environment tests
-        conditionizr.config({
-            assets: '<?php echo get_template_directory_uri(); ?>',
-            tests: {}
-        });
+//        conditionizr.config({
+//            assets: '<?php //echo get_template_directory_uri(); ?>//',
+//            tests: {}
+//        });
         </script>
 
 	</head>
@@ -34,17 +41,17 @@
 			<header class="header clear" role="banner">
 
 					<!-- logo -->
-					<div class="logo">
-						<a href="<?php echo home_url(); ?>">
-							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
-						</a>
-					</div>
+<!--					<div class="logo">-->
+<!--						<a href="--><?php //echo home_url(); ?><!--">-->
+<!--							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->-->
+<!--							<img src="--><?php //echo get_template_directory_uri(); ?><!--/img/logo.svg" alt="Logo" class="logo-img">-->
+<!--						</a>-->
+<!--					</div>-->
 					<!-- /logo -->
 
 					<!-- nav -->
 					<nav class="nav" role="navigation">
-						<?php html5blank_nav(); ?>
+<!--						--><?php //html5blank_nav(); ?>
 					</nav>
 					<!-- /nav -->
 
