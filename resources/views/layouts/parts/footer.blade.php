1c9fb2ada6002eb2c3e6bf6aea5fd2431275953b
Alex Velikanov <biz@alexvelikanov.com>
2015-12-23 04:28:10 +0700

    Frontend - About Us footer styles

--- resources/views/layouts/parts/footer.blade.php
+++ resources/views/layouts/parts/footer.blade.php
@@ -1,3 +1,42 @@
+<footer class="about-footer">
+    <div class="container">
+        <div class="col-sm-3">
+            <h4 class="blue">About Ideaing</h4>
+            <ul>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Terms of Use</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Copyright</a></li>
+            </ul>
+        </div>
+        <div class="col-sm-3">
+            <h4 class="green">Explore</h4>
+            <ul>
+                <li><a href="#">Share your ideas</a></li>
+                <li><a href="#">Sell on Ideaing</a></li>
+                <li><a href="#">Professional Installers</a></li>
+            </ul>
+        </div>
+        <div class="col-sm-3 social-connect">
+            <h4 class="orange">Let's connect</h4>
+            <ul>
+                <li><a class="fb" href="#"><span>Like <b class="count">12k</b></span></a></li>
+                <li><a class="twi" href="#"><span>Follow  <b class="count">12k</b></span></a></li>
+                <li><a class="gp" href="#"><span>Follow <b class="count">12k</b></span></a></li>
+                <li><a class="pint" href="#"><span>Follow <b class="count">12k</b></span></a></li>
+                <li><a class="inst" href="#"><span>Follow <b class="count">12k</b></span></a></li>
+            </ul>
+        </div>
+        <div class="col-sm-3">
+            <h4 class="pink">Ideas to You</h4>
+            <input class="form-control" type="text" placeholder="Email address">
+            <input class="form-control" type="submit" value="Submit">
+        </div>
+    </div>
+</footer>
+
+
 <div id="myModal" class="modal col-lg-3 col-md-4 col-sm-5 col-xs-12 col-offset-md-2">
     <div class="modal-content hero-box qiuck-signup modal-login">
         <form>
