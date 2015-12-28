9aae11a323605e95ced6e616c1e0c15b055be072
Alex Velikanov <biz@alexvelikanov.com>
2015-12-28 20:06:02 +0700

    Products front end - All markup

--- resources/views/static/product-details.blade.php
+++ resources/views/static/product-details.blade.php
@@ -22,7 +22,7 @@
     </nav>
 
     <div class="container full-620 main-container fixed-sm">
-        <article class="col-sm-11 col-md-offset-1">
+        <article class="col-sm-11 col-md-offset-1 product">
 
             <aside class="share-bar sticks-on-scroll">
                 <ul>
@@ -36,22 +36,149 @@
             </aside>
 
             <section class="article-content">
+                <p>
+                    Nest Cam and IFTTT, this $99/£89 detector is the best connected one we've seen yet. Nest Cam and IFTTT, this $99/£89 Nest Cam and IFTTT, this $99/£89.  Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?  Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+                </p>
+                <p>
+                    Nest Cam and IFTTT, this $99/£89 detector is the best connected one we've seen yet. Nest Cam and IFTTT, this $99/£89 Nest Cam and IFTTT, this $99/£89.  Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?  Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+                </p>
+                <p>
+                    Nest Cam and IFTTT, this $99/£89 detector is the best connected one we've seen yet. Nest Cam and IFTTT, this $99/£89 Nest Cam and IFTTT, this $99/£89.  Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?  Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+                </p>
+                <p>
+                    Nest Cam and IFTTT, this $99/£89 detector is the best connected one we've seen yet. Nest Cam and IFTTT, this $99/£89 Nest Cam and IFTTT, this $99/£89.  Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?  Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+                </p>
             </section>
 
+            <section class="grey-bg product-specs">
+                <h3>Specifications</h3>
+                <p>
+                    Nest Cam and IFTTT, this $99/£89 detector is the best connected one we've seen yet. Nest Cam and IFTTT, this $99/£89 Nest Cam and IFTTT, this $99/£89.  Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+                </p>
+
+                <div class="card small col-lg-3">
+                    <h4>Type</h4>
+                    Wireless Speakers
+                </div>
+                <div class="card small col-lg-3">
+                    <h4>Connections</h4>
+                    3.5mm stereo
+                </div>
+                <div class="card small col-lg-3">
+                    <h4>Dimensions</h4>
+                    8.03 x 14.33
+                </div>
+                <div class="card small col-lg-3">
+                    <h4>Weight</h4>
+                    14lbs
+                </div>
+
+                <div class="card small col-lg-3">
+                    <h4>Announced</h4>
+                    Sept 29 2015
+                </div>
+                <div class="card big col-lg-6">
+                    <h4>Pricing and Availiability</h4>
+                    <ul>
+                        <li><b>Avaliability</b> <hr> Pre-release</li>
+                        <li><b>Announced (US)</b> <hr> September 29, 2015</li>
+                        <li><b>Original Price</b> <hr> $499.99</li>
+                    </ul>
+                </div>
+                <div class="card small col-lg-3">
+                    <h4>Original Site Details</h4>
+                    <a href="#">
+                        www.sonos.com
+                    </a>
+                </div>
+
+            </section>
+
+            <section class="purple">
+                <h3>Comparisons</h3>
+
+                <button class="arrow arrow-left"></button>
+
+                <div class="col-lg-3 comparison-tab">
+                    <section class="search-bar">
+                        <a href="#" class="search-toggle" data-toggle=".mobile-search-bar">Search</a>
+                        <input class="form-control  hidden-620" type="text" name="search" value="Search..."/>
+                    </section>
+                </div>
+                <div class="col-lg-3 comparison-tab">
+                    <img class="img-responsive"/>
+                    <h4>Next Project <br>(second generation)</h4>
+                    <i>Announced 29 October 2015</i>
+                    <b class="score">9.0</b>
+                    <div class="star-raiting">
+                        <span class="starts">(543)</span>
+                    </div>
+                    <div class="purple-bg price-badge">
+                        Amazon - <b>$375</b>
+                    </div>
+                    <a class="btn-none">More-info</a>
+                </div>
+                <div class="col-lg-3 comparison-tab">
+                </div>
+
+                <button class="arrow arrow-right"></button>
+
+                <div class="crearfix"></div>
+                <h5>Compared</h5>
+
+                <div class="col-lg-3 comparison-tab table-heads">
+                    <img class="img-responsive"/>
+                    <h4>Next Project <br>(second generation)</h4>
+                    <hr>
+
+                    <b>Connections</b>
+                    <b>Original Pricing</b>
+                    <b>Pricing Range</b>
+                    <b>Dimensions</b>
+                    <b>Weight</b>
+                </div>
+                <div class="col-lg-3 comparison-tab table-cells">
+                    <img class="img-responsive"/>
+                    <h4>Next Project <br>(second generation)</h4>
+                    <hr>
+
+                    <b>3.5mm stereo</b>
+                    <b>$375.00</b>
+                    <b>$375.00 - $500</b>
+                    <b>8.03 x 14.33</b>
+                    <b>14 lbx</b>
+                </div>
+            </section>
+            <!-- TODO - use two (three?) columns -->
+
+            <h3>Reviews (4)</h3>
+
+            <div class="col-sm-3">
+                <h6>Critic Reviews</h6>
+                <b class="score pink">
+                    9.8
+                </b>
+            </div>
+            <div class="col-sm-9">
+                <table class="rating-lines">
+                    <tbody>
+                        <tr class="name">
+                            <td class="name">CNET</td>
+                            <td class="line"></td>
+                            <td class="score">9.0</td>
+                        </tr>
+                        <!-- TODO - use 4 rows -->
+
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-sm-3 col-md-offset-3">
+                <b class="qu">Every home needs this</b>
+                <author>Pete McBride</author>
+                <i>CNET Editor</i>
+            </div>
         </article>
     </div>
-
-
-    <aside class="share-bar sticks-on-scroll">
-        <ul>
-            <li class="fb"><a href="#">55</a></li>
-            <li class="twi"><a href="#">120</a></li>
-            <li class="gp"><a href="#">521</a></li>
-            <li class="email"><a href="#">Email</a></li>
-            <li class="heart"><a href="#">12.5</a></li>
-            <li class="comment"><a href="#">322</a></li>
-        </ul>
-    </aside>
 
 
     <section class="comments">
