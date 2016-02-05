fb2d81fd944141cf4ad77b928d1094b0aa654134
cianariel <cianariel72@gmail.com>
2016-02-05 19:07:07 +0100

    Fix home page

--- resources/views/layouts/parts/header.blade.php
+++ resources/views/layouts/parts/header.blade.php
@@ -3,67 +3,75 @@
                     <h2 id="site-name">Ideaing | Ideas for Smarter Living</h2>
                     <nav id="top-nav" class="row">
                         <div class="container full-sm fixed-sm">
-                            <div class="mobile-menu-switch hidden shown-620" data-toggle=".mobile-menu" data-overlay="true"></div>
-                            <div class="mobile-menu col-xs-8 hidden-soft">
-                                <ul>
-                                    <li><a class="ideas-link" href="#">Ideas</a></li>
-                                    <li><a class="products-link" href="#">Shop</a></li>
-                                    <li><a class="photos-link" href="#">Discuss</a></li>
-
-                                </ul>
-                            </div>
-                            <div  class="col-xs-5 category-menu hidden-620">
-                                <ul>
-                                    <li>
-                                        <a class="ideas" href="#">
-                                            <i class="m-icon m-icon--bulb"></i>&nbsp;
-                                            <span class="m-icon-text">Ideas</span>
-                                            <span class="box-link-active-line"></span>
-                                        </a>
-                                    </li>
-                                    <li class="nested">
-                                        
-                                        <a class="shop" data-toggle=".shop-menu" href="#"><i class="m-icon m-icon--item"></i>&nbsp;
-                                            <span class="m-icon-text">Shop</span> 
-                                            <span class="box-link-active-line"></span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a class="disc" href="#"><i class="m-icon m-icon--discuss-active"></i>&nbsp;
-                                            <span class="m-icon-text">Discuss</span>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-
-                            <div class="col-xs-3 logo-wrap">
-                                <a id="ideaing-logo" class="center-block" href="/">
-                                    <img src="/assets/images/logo.png" class="img-responsive " alt="">
-                                    <span class="scroll-logo"></span>
-                                    <img src="/assets/images/logo-hover.png" class="img-responsive " alt="">
-                                </a>
-                            </div>
-
-                            <div class="">
-                                <!--<div  class="col-md-4 col-sm col-xs-4 category-menu mobile-category-menu hidden-xs hidden shown-620">
-                                    <ul>
-                                        <li class="nested"><a class="shop" href="">Shop</a></li>
-                                        <li><a class="disc" href="">Discuss</a></li>
-                                        <li><a class="ideas" href="">Ideas</a></li>
-                                    </ul>
-                                </div>-->
-
-                                <div class="col-md-1 col-xs-2 pull-right signin">
-                                    <a data-toggle="modal" data-target="#myModal" href="#"><i class="m-icon m-icon--user"></i> Log in</a>
+                            <div class="top-nav-holder">
+                                <div class="visible-xs col-xs-1">
+                                    <div class="mobile-menu-switch " data-toggle=".mobile-menu" data-overlay="true"></div>
                                 </div>
-                                <section class="search-bar col-xs-2 col-md-2 pull-right">
-                                
-                                    <a href="#" class="search-toggle" data-toggle=".mobile-search-bar"><i class="m-icon m-icon--search-id"></i></a>
-                                    <span class="search-input-holder">
-                                        <i class="m-icon m-icon--search-id"></i>
-                                        <input class="form-control  hidden-620" type="text" name="search" placeholder="Search..."/>
-                                    </span>
+                                <div class="mobile-menu col-xs-8 hidden-soft">
+                                    <ul>
+                                        <li><a class="ideas" href="#"><i class="m-icon m-icon--bulb"></i>&nbsp; Ideas</a></li>
+                                        <li><a class="shop" href="#"><i class="m-icon m-icon--item"></i>&nbsp; Shop</a></li>
+                                        <li><a class="disc" href="#"><i class="m-icon m-icon--discuss-active"></i>&nbsp; Discuss</a></li>
+
+                                    </ul>
+                                </div>
+                                <div  class="col-sm-5 category-menu hidden-xs">
+                                    <ul>
+                                        <li>
+                                            <a class="ideas" href="#">
+                                                <i class="m-icon m-icon--bulb"></i>
+                                                <span class="m-icon-text">Ideas</span>
+                                                <span class="box-link-active-line"></span>
+                                            </a>
+                                        </li>
+                                        <li class="nested">
+                                            
+                                            <a class="shop" data-toggle=".shop-menu" href="#"><i class="m-icon m-icon--item"></i>
+                                                <span class="m-icon-text">Shop</span> 
+                                                <span class="box-link-active-line"></span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a class="disc" href="#"><i class="m-icon m-icon--discuss-active"></i>&nbsp;
+                                                <span class="m-icon-text">Discuss</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+
+                                <div class="col-xs-6 col-sm-3 text-center">
+                                    <a id="ideaing-logo" class="center-block" href="/">
+                                        <i class="m-icon m-icon--logo-without-text-blue">
+                                            <span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span><span class="path9"></span><span class="path10"></span><span class="path11"></span><span class="path12"></span><span class="path13"></span><span class="path14"></span><span class="path15"></span><span class="path16"></span><span class="path17"></span><span class="path18"></span>
+                                        </i>
+    <!--                                    <img src="/assets/images/logo.png" class="img-responsive " alt="">-->
+                                        <i class="m-icon m-icon--bulb2 scroll-logo">
+                                            <span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span><span class="path9"></span><span class="path10"></span>
+                                        </i>
+                                        <i class="m-icon m-icon--logo-without-text-red">
+                                            <span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span><span class="path9"></span><span class="path10"></span><span class="path11"></span><span class="path12"></span><span class="path13"></span><span class="path14"></span><span class="path15"></span><span class="path16"></span><span class="path17"></span><span class="path18"></span>
+                                        </i>
+    <!--                                    <img src="/assets/images/logo-hover.png" class="img-responsive " alt="">-->
+                                    </a>
+                                </div>
+
+                                <section class="search-bar col-xs-2 col-sm-2 ">
+                                    
+                                    <div class="row">
+                                        <a href="#" class="search-toggle visible-xs" data-toggle=".mobile-search-bar"><i class="m-icon m-icon--search-id"></i></a>
+                                        <span class="search-input-holder visible-sm visible-md visible-lg">
+                                            <i class="m-icon m-icon--search-id"></i>
+                                            <input class="form-control  " type="text" name="search" placeholder="Search..."/>
+                                        </span>
+                                    </div>
                                 </section>
+
+                                <div class="col-xs-3 col-sm-2  signin">
+                                    <div class="row">
+                                        <a data-toggle="modal" data-target="#myModal" href="#"><i class="m-icon m-icon--user"></i> Log in</a>
+                                    </div>
+                                </div>
+                                <div class="clearfix"></div>
                             </div>
                         </div>
                     </nav>
@@ -215,7 +223,7 @@
                 </div>
             </nav>
         </header>
-        <nav class="mid-nav rooms hidden-620">
+        <nav class="mid-nav rooms hidden-xs">
             <div class="container full-sm fixed-sm">
                 <ul class="wrap col-xs-9">
                     <li class="home ">
@@ -223,18 +231,19 @@
                         <a href=""><i class="m-icon m-icon--smart-home"></i> Smart Home</a>
                     </li>
                     <li><a href="">Kitchen</a></li>
+                    <li><a href="">Bath</a></li>
                     <li><a href="">Bedroom</a></li>
                     <li><a href="">Office</a></li>
                     <li><a href="">Living</a></li>
                     <li><a href="">Outdoor</a></li>
                     <li><a href="">Lighting</a></li>
                     <li><a href="">Decor</a></li>
-                    <li><a data-toggle=".extra-nav" class="more-link extra" href="">...</a>
+                    <!--<li><a data-toggle=".extra-nav" class="more-link extra" href="">...</a>
                         <ul class="extra-nav hidden-620 hidden-soft">
                             <li><a class="travel-link blue" href="#">Travel</a></li>
                             <li><a class="wearables-link green" href="#">Wearables</a></li>
                         </ul>
-                    </li>
+                    </li>-->
 
                 </ul>
             </div>
