184433407c30f3ed5d00cd0b8821583931379659
cianariel <cianariel72@gmail.com>
2016-02-12 02:55:23 +0100

    Create the profile page

---
+++ resources/views/user/parts/feed.blade.php
@@ -0,0 +1,122 @@
+        <div class="container">
+            <div class="col-md-3 side-bar hidden-xs hidden-sm">
+                <div class="row">
+                    <ul class="nav sidenav">
+                        <li class="active">
+                            <a href="#">
+                                <i class="m-icon m-icon--menu"></i>&nbsp;
+                                All Activity
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="m-icon m-icon--heart-id"></i>&nbsp;
+                                Likes
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="m-icon m-icon--comments-id"></i>&nbsp;
+                                Comments
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="m-icon m-icon--menu"></i>&nbsp;
+                                Saved
+                            </a>
+                        </li>
+                    </ul> 
+                </div>
+            </div>
+            <div class="col-md-9 main-content">
+                <div class="feed-content row">
+                    <div class="feed-header ">
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <div class="pull-left">
+                                    <img src="{{$profile}}" width="50px" class="profile-photo" alt="">
+                                </div>
+                                <div class="pull-left name-time">
+                                    <strong>{{$fullname}}</strong> <i class="m-icon--heart-solid"></i><br>
+                                    <span class="time">Yesterday at 14:45pm</span>
+                                </div>
+                                <div class="pull-right">
+                                    <a href="#">
+                                        Actions 
+                                        <span class="caret"></span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="feed-body ">
+                        <div class="row">
+                            <div class="col-xs-4">
+                                <img src="/assets/images/feed1.jpg">
+                            </div>
+                            <div class="col-xs-8">
+                                <strong>You recently liked a story</strong>: Epic samle happneing right now of all Apple devices in the 2015 Festive season across the boards!
+                            </div>
+                        </div>
+                    </div>
+                    <div class="feed-footer ">
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <div class="pull-left">
+                                    <span class="favorite"><i class="m-icon--heart-solid"></i> 2133</span>
+                                    <span class="comment"><i class="m-icon--buble"></i> 758</span>
+                                </div>
+                                <div class="pull-right">
+                                    <a href="#"><strong>View original post</strong></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="feed-content row">
+                    <div class="feed-header ">
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <div class="pull-left">
+                                    <img src="{{$profile}}" width="50px" class="profile-photo" alt="">
+                                </div>
+                                <div class="pull-left name-time">
+                                    <strong>{{$fullname}}</strong> <i class="m-icon--Saved-Active"></i><br>
+                                    <span class="time">Yesterday at 14:45pm</span>
+                                </div>
+                                <div class="pull-right">
+                                    <a href="#">
+                                        Actions 
+                                        <span class="caret"></span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="feed-body ">
+                        <div class="row">
+                            <div class="col-xs-4">
+                                <img src="/assets/images/feed1.jpg">
+                            </div>
+                            <div class="col-xs-8">
+                                <strong>You recently liked a story</strong>: Epic samle happneing right now of all Apple devices in the 2015 Festive season across the boards!
+                            </div>
+                        </div>
+                    </div>
+                    <div class="feed-footer ">
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <div class="pull-left">
+                                    <span class="favorite"><i class="m-icon--heart-solid"></i> 2133</span>
+                                    <span class="comment"><i class="m-icon--buble"></i> 758</span>
+                                </div>
+                                <div class="pull-right">
+                                    <strong>From $350.00 on &nbsp;</strong><img src="/assets/images/dummies/amazon-black.png" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
--- resources/views/user/user-profile.blade.php
+++ resources/views/user/user-profile.blade.php
@@ -1,6 +1,95 @@
 @extends('layouts.main')
 
+@section('body-class'){{ 'profilepage' }}@stop
 
 @section('content')
-user profile
+    <div ng-app="pagingApp" ng-controller="pagingController">
+        <section id="hero" class="landing-hero">
+            <div class="hero-background" style="background-image: url('/assets/images/landing-hero-3.jpg');"></div>
+            <div class="color-overlay"></div>
+            <div class="container">
+                <div class="col-sm-6 text-right">
+                    <img class="profile-photo" width="120px" src="{{$profile}}">
+                </div>
+                <div class="col-sm-6">
+                    <p>
+                        <span class="fullname">{{$fullname}}</span>&nbsp;
+                        <span class="location"><i class=" m-icon--Location"></i> Oklahoma, USA</span>
+                    </p>
+                    <p class="description">Only the best tech gadgets from Apple. Samsung and LG.</p>
+                    <div>
+                        <button id="btn-follow" type="button" class="btn " uib-dropdown-toggle>
+                            Follow <i class=" m-icon--Actions-Down-Arrow-Active"></i>
+                        </button>
+                        <button id="btn-add-friend" type="button" class="btn btn-primary" >
+                            <i class="m-icon m-icon--Add-Friends"></i>&nbsp; Add Friends 
+                        </button>
+                    </div>
+                    <br>
+                    <div>
+                        <a href="#" class="follow">1550 Followers</a>
+                        <a href="#" class="follow">91 Following</a>
+                    </div>
+                </div>
+            </div>
+            <a href="#" id="upload-photo" ><i class="m-icon--Upload-Inactive"></i> Upload Photo</a>
+            <div class="edit-background hidden-xs hidden-sm">
+                <a href="#">
+                    <i class="m-icon--Edit-Background"></i><br>
+                    Edit background
+                </a>
+            </div>
+            <div class="hidden-xs hidden-sm edit-profile">
+                <div><a href="#" class="edit-profile-link" ng-click="openProfileSetting()">Edit Profile&nbsp;&nbsp;<i class="m-icon--Edit-Profile"></i></a></div>
+                <p><a href="#">View your profile as other people see it</a></p>
+            </div>
+        </section>
+        <div class="app-wrap" >
+            <div class="container ">
+                <nav id="hero-nav" >
+                    <ul class="breadcrumb pull-left hidden-xs hidden-sm">
+                        <li><a href="/"><i class="m-icon--Home"></i> Home</a> <span>-></span></li>
+                        <li><a href="/user/profile"><img class="profile-photo" width="40px" src="{{$profile}}" alt=""> My Profile</a></li>
+                    </ul>
+                    <ul class=" main-content-filter pull-right">
+                        <li ng-class="{active: (activeMenu == '1' || !activeMenu)}" ng-click="activeMenu='1'">
+                            <a ng-click="filterContent(null)"  href="" data-filterby="all" class="all-activity">
+                                <i class="m-icon m-icon--menu"></i>
+                                All Activity
+                                
+                            </a>
+                        </li>
+                        <li ng-class="{active: activeMenu == '2'}" ng-click="activeMenu='2'">
+                            <a href="/user/profile/my-posts" class="my-post">
+                                <i class="m-icon m-icon--image"></i>
+                                My Posts
+                            </a>
+                        </li>
+                        <li ng-class="{active: activeMenu == '3'}" ng-click="activeMenu='3'">
+                            <a  ng-click="filterContent('product')" data-filterby="products" href="" class="message">
+                                <i class="m-icon m-icon--Messages"></i>
+                                Messages
+                            </a>
+                        </li>
+                        <li ng-class="{active: activeMenu == '4'}" ng-click="activeMenu='4'">
+                            <a data-filterby="photos" href="" class="my-product">
+                                <i class="m-icon m-icon--shopping-bag-light-green"></i>
+                                My Products
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="clearfix"></div>
+            <br><br>
+            @if($permalink == 'my-posts')
+                @include('user.parts.post')
+            @else
+                @include('user.parts.feed')
+            @endif
+        </div>
+    </div>
+    <script src="/assets/js/vendor/angular-busy.min.js"></script>
+    <script src="/assets/js/angular-custom/custom.paging.js"></script>
+    <script src="/assets/js/angular-custom/public.common.js"></script>
 @stop
