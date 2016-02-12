14e870cd48513be9a0fb61de95c58e5c19b51a5f
Alex Velikanov <biz@alexvelikanov.com>
2016-02-13 00:10:28 +0800

    Shop - alpha fetch products for the Shop menu

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -6,12 +6,54 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 
 class ShopController extends Controller
 {
     public function index()
     {
-        return view('shop.index');
+        $menuProductSettings = [
+            'ActiveItem' => true,
+            'limit'      => 4,
+            'page'       => 1,
+            'CustomSkip' => false,
+
+            'CategoryId' => false,
+            'FilterType' => false,
+            'FilterText' => false,
+            'ShowFor'    => false,
+            'WithTags'   => false,
+        ];
+
+        $prod = new Product();
+
+        $menuProductSettings['CategoryId'] = 1;
+        $travel = $prod->getProductList($menuProductSettings);
+        $menuProducts['travel'] = $travel['result'];
+        $menuProductSettings['IgnoreIDs'] = $travel['allIDs'];
+
+        $menuProductSettings['CategoryId'] = 2;
+        $wearables = $prod->getProductList($menuProductSettings);
+        $menuProducts['wearables'] = $wearables['result'];
+        $menuProductSettings['IgnoreIDs'] = $menuProductSettings['IgnoreIDs'] + $wearables['allIDs'];
+
+        $menuProductSettings['CategoryId'] = 3;
+        $homeDecor = $prod->getProductList($menuProductSettings);
+        $menuProducts['homeDecor'] = $homeDecor['result'];
+        $menuProductSettings['IgnoreIDs'] = $menuProductSettings['IgnoreIDs'] + $wearables['allIDs'];
+
+        $menuProductSettings['CategoryId'] = 5;
+        $menuProductSettings['limit'] = 8;
+        $smartHome = $prod->getProductList($menuProductSettings);
+        $menuProducts['smartHome'] = $smartHome['result'];
+        $menuProductSettings['IgnoreIDs'] = $menuProductSettings['IgnoreIDs'] + $wearables['allIDs'];
+
+        $menuProductSettings['CategoryId'] = 7;
+        $menuProductSettings['limit'] = 1;
+        $mostPopular = $prod->getProductList($menuProductSettings);
+        $menuProducts['mostPopular'] = $mostPopular['result'];
+
+        return view('shop.index')->with('menuProducts', $menuProducts);
     }
 
     public function shopCategory()
