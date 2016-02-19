f5d66145717dc1d91849ca8af00553f3ee93d1c1
Tanvir Anowar <tanvir@carobn51.com>
2016-02-19 18:37:31 +0600

    shop auth. Fix.

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -10,10 +10,23 @@ use App\Models\Product;
 use App\Models\ProductCategory;
 
 
-class ShopController extends Controller
+class ShopController extends ApiController
 {
+    public function __construct()
+    {
+        //check user authentication and get user basic information
+        $this->authCheck = $this->RequestAuthentication(array('admin','editor','user'));
+
+    }
+
     public function index($category = false)
     {
+
+        $userData = '';
+        if ($this->authCheck['method-status'] == 'success-with-http') {
+            $userData = $this->authCheck['user-data'];
+        }
+
         if($category){
             if(!$categoryModel = ProductCategory::where('extra_info', $category)->first()){
                 return redirect('/shop/');
@@ -22,6 +35,7 @@ class ShopController extends Controller
             $parentCategoryName =  @ProductCategory::where('id', $categoryModel->parent_id)->first()->category_name;
 
             return view('shop.shop-category')
+                ->with('userData',$userData)
                 ->with('currentCategory', $category)
                 ->with('parentCategory', $parentCategoryName)
                 ;
@@ -29,6 +43,7 @@ class ShopController extends Controller
             $categoryTree = ProductCategory::buildCategoryTree();
 
             return view('shop.index')
+                ->with('userData',$userData)
                 ->with('categoryTree', $categoryTree)
                 ;
         }
