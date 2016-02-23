a13d101f48a5f551dfc4b49e2e1c92e2f8e97c43
Alex Velikanov <biz@alexvelikanov.com>
2016-02-23 17:47:00 +0700

    Shop filter -- Show only parent master category

--- resources/views/shop/filter-menu.blade.php
+++ resources/views/shop/filter-menu.blade.php
@@ -1,8 +1,8 @@
 <aside class="room-filter">
     <ul class="extra-nav hidden-620">
-        @foreach($categoryTree as $name => $unused)
-            <li><a class="{{$name}}-link" href="#">{{$name}}</a></li>
-        @endforeach
+{{--        @foreach($categoryTree as $name => $unused)--}}
+            <li><a class="{{@$parentCategory->extra_info ?: $currentCategory->extra_info}}-link" href="/shop/{{@$parentCategory->extra_info ? $parentCategory . '/' : ''}}{{$currentCategory->extra_info}}">{{@$parentCategory->category_name  ?: $currentCategory->category_name}}</a></li>
+        {{--@endforeach--}}
     </ul>
 
     <ul class="room-list">
@@ -15,19 +15,19 @@
         @endforeach
     </ul>
 
-    <ul class="sortby">
-        <li ng-click="filterPlainContent(false, 'sale_price')">Price</li>
-    </ul>
+    {{--<ul class="sortby">--}}
+        {{--<li ng-click="filterPlainContent(false, 'sale_price')">Price</li>--}}
+    {{--</ul>--}}
 
-    <h6 class="gift">Gift ideas</h6>
-    <ul class="for">
-        <li>For Her</li>
-        <li>For Him</li>
-        <li>For Kids</li>
-        <li>For Pets</li>
-        <li>For the Techie</li>
-        <li>For the Traveler</li>
-        <li>For the Decorator</li>
-        <li>Stocking Suffers</li>
-    </ul>
+    {{--<h6 class="gift">Gift ideas</h6>--}}
+    {{--<ul class="for">--}}
+        {{--<li>For Her</li>--}}
+        {{--<li>For Him</li>--}}
+        {{--<li>For Kids</li>--}}
+        {{--<li>For Pets</li>--}}
+        {{--<li>For the Techie</li>--}}
+        {{--<li>For the Traveler</li>--}}
+        {{--<li>For the Decorator</li>--}}
+        {{--<li>Stocking Suffers</li>--}}
+    {{--</ul>--}}
 </aside>
