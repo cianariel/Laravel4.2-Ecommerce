1e26fa52c0cb089c12bd02a680b2fd92f0c3e4f0
cianariel <cianariel72@gmail.com>
2016-02-28 04:48:16 +0100

    Fix a few issues

--- resources/views/layouts/aboutus.blade.php
+++ resources/views/layouts/aboutus.blade.php
@@ -15,7 +15,7 @@
         <div><h4>Get inspiration</h4></div>
         <div><img src = '/assets/images/about1.png' class = 'para1_image'></div>
         <div class = 'para1_content'>Get inspiration on making your home smarter. We've created a platform that will inspire you to make your home smarter. We'll recommend &amp; demonstrate the most cutting edge products designed to make your home more efficient &amp; more secure. thus making your life easier.</div>
-        <div class = 'para1_button'><a class="btn btn-success col-xs-12" href="#">Get started</a></div>
+                <div class = 'para1_button'><a class="btn btn-success col-xs-12" href="{{url('signup')}}">Get started</a></div>
     </div>
 </div>
 
@@ -24,7 +24,7 @@
         <div class = 'space'></div>
         <div style = 'color:white'><h4>Discover the collest and most unique home decor</h4></div>
         <div class = 'para1_content' style = 'color:white'>At ideaing, we believe your home should look its best. We scour thousands of products from Amazon to major online retailers, and curate only the best &amp; most unique home decor.</div>
-        <div class = 'para1_button'><a class="btn btn-success col-xs-12" href="#">Get started</a></div>
+                <div class = 'para1_button'><a class="btn btn-success col-xs-12" href="{{url('/')}}">Get started</a></div>
         <div class = 'para2_space'></div>
     </div>
 </div>
@@ -36,7 +36,6 @@
             <div class = 'space'></div>
 	        <div class = ''>We are passionate about the connected home and interior design. and we want you to be part of our vision. Vote on your favourite products and ideas.</div>
             <div class = 'space'></div>
-	       <div><a class="btn btn-success col-xs-12" href="#">Get started</a></div>
         </div>
     </div>
 
@@ -46,7 +45,6 @@
             <div class = 'space'></div>
             <div>Get inspiration from others and share your home ideas to make our homes sexier and smarter than ever before.</div>
             <div class = 'space'></div>
-            <div><a class="btn btn-success col-xs-12" href="#">Get started</a></div>
         </div>
     </div>
 </div>
@@ -57,7 +55,7 @@
         <div><h4>Best Deals</h4></div>
     
         <div class = 'para1_content'>Find the best deals for home tech</div>
-        <div class = 'para1_button'><a class="btn btn-success col-xs-12" href="#">Get started</a></div>
+                <div class = 'para1_button'><a class="btn btn-success col-xs-12" href="{{url('shop')}}">Get started</a></div>
         <div><img src = '/assets/images/about3.png' style = 'max-width:100%; max-height:500px'></div>
     </div>
 </div>
