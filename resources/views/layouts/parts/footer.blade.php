0cda6b06542144b1826b414450d33f730df54f34
Alex Velikanov <biz@alexvelikanov.com>
2016-03-02 02:11:07 +0700

    ++

--- resources/views/layouts/parts/footer.blade.php
+++ resources/views/layouts/parts/footer.blade.php
@@ -26,7 +26,7 @@
                 <li><a class="twi" href="#"><span><i class="m-icon--twitter-id"></i> Follow  <b class="share-count twi count"></b></span></a></li>
                 <li><a class="gp" href="#"><span><i class="m-icon--google-plus-id"></i> Follow <b class="share-count gp count"></b></span></a></li>
                 <li><a class="pint" href="#"><span><i class="m-icon--pinterest-id"></i> Follow <b class="share-count pint count"></b></span></a></li>
-                <li><a class="inst" href="#"><span><i class=" m-icon--footer-instagram"></i> Follow <b class="count">12k</b></span></a></li>
+                <li><a class="inst" href="#"><span><i class=" m-icon--footer-instagram"></i> Follow <b class="count"></b></span></a></li>
             </ul>
         </div>
         <div class="col-sm-3 col-xs-12">
