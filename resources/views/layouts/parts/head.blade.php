cfc1918eaef085a161933deaa9dbf2a8f263e322
Alex Velikanov <biz@alexvelikanov.com>
2016-03-03 03:52:27 +0700

    Fix google analytics JS

--- resources/views/layouts/parts/head.blade.php
+++ resources/views/layouts/parts/head.blade.php
@@ -84,6 +84,17 @@ if (screen.width < 992 && screen.width > 620) {
 
 <meta name="google-site-verification" content="xiWn24mA3aZopoTkElR97n-HdvsfctffW493Q6x_vZU" />
 
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-70081627-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
+
 <!-- Facebook Pixel Code -->
 <script>
     !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
