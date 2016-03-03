338be6f457c3d6d6f565024f34bea4fbf2353757
Alex Velikanov <biz@alexvelikanov.com>
2016-03-04 01:00:17 +0700

    CMS -- Fix WP header

--- public/ideas/wp-content/themes/ideaing/header.php
+++ public/ideas/wp-content/themes/ideaing/header.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html <?php language_attributes(); ?> class="no-js idea-stories">
 	<head>
-
+        <title><?php the_title()?></title>
 		<?php wp_head(); ?>
 
         <?php loadLaravelView('head'); ?>
