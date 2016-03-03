066d726c4b62b5fa091d9eceff684425916c6873
Alex Velikanov <biz@alexvelikanov.com>
2016-03-04 03:08:32 +0700

    Product sticky header sharing fix

--- resources/views/product/product-details.blade.php
+++ resources/views/product/product-details.blade.php
@@ -67,14 +67,14 @@
                 <li class="all-shares"><b class="share-count all"></b> <br/>all shares</li>
                 <li><a class="fb" href="#"
                        onclick="javascript: (function() {
-							   window.open('https://www.facebook.com/sharer/sharer.php?u=' +  thisUrl, 'fbwindow', 'width=600, height=360, left=250, top=150, toolbar=0, location=0, menubar=0, scrollbars=0');})();">
+							   window.open('https://www.facebook.com/sharer/sharer.php?u=' +  window.location.host + window.location.pathname, 'fbwindow', 'width=600, height=360, left=250, top=150, toolbar=0, location=0, menubar=0, scrollbars=0');})();">
                         <i class="m-icon m-icon--facebook-id">
 
                         </i> <b class="fb share-count"></b></a></li>
 
                 <li><a class="twi" href="#"
                        onclick="javascript: (function() {
-                               window.open('https://twitter.com/share?url=' + thisUrl + '&counturl=' + thisUrl + '&text=@Ideaing', 'fbwindow', 'width=600, height=360, left=250, top=150, toolbar=0, location=0, menubar=0, scrollbars=0');})();">
+                               window.open('https://twitter.com/share?url=' + window.location.host + window.location.pathname + '&counturl=' + thisUrl + '&text=@Ideaing', 'fbwindow', 'width=600, height=360, left=250, top=150, toolbar=0, location=0, menubar=0, scrollbars=0');})();">
                         <i class="m-icon  m-icon--twitter-id"></i> <b class="twi share-count"></b></a></li>
             </ul>
 
