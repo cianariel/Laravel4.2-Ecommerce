7d0b211b429392ae0c08a7629ccca906648e2429
Alex Velikanov <biz@alexvelikanov.com>
2016-03-07 22:51:01 +0700

    alpha all ideas views

--- public/ideas/wp-content/themes/ideaing/index.php
+++ public/ideas/wp-content/themes/ideaing/index.php
@@ -1,21 +1,33 @@
 @include('header')
-	<main role="main">
-		<!-- section -->
-		<section>
 
-			<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
+<div class="app-wrap" id="pagingApp" ng-app="pagingApp" ng-controller="pagingController" ng-cloak>
 
-			<?php get_template_part('loop'); ?>
+    <div class="clearfix"></div>
 
-			<?php get_template_part('pagination'); ?>
+    <div class="homepage-grid center-block" style="min-height:1000px">
+        <div class="loader loader-abs" cg-busy="firstLoad"></div>
+        {{--<div class="loader loader-abs" cg-busy="filterLoad"></div>--}}
+        <div class="loader loader-fixed" cg-busy="nextLoad"></div>
 
-		</section>
-		<!-- /section -->
-	</main>
+        <div ng-repeat="batch in content" class="main-content col-xs-12">
+            <div class="grid-box-3">
+                <div class="box-item idea-box" ng-if="item.type == 'idea'" ng-repeat="item in batch['row-1']">
+                    <a href="{{item.url}}" class="box-item__label">{{renderHTML(item.title)}}</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <a ng-click="loadMore()" class="btn btn-success bottom-load-more col-xs-12">Load More</a>
+    </div>
 
-<?php get_sidebar(); ?>
+</div>
+
 
 <?php get_footer(); ?>
+
+<script type="text/javascript" src="/assets/product/js/custom.product.js"></script>
+
 
 
 
