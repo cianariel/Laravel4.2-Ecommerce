2a1b2e1a7eee3b2ec6b29c76df5918d716274376
Alex Velikanov <biz@alexvelikanov.com>
2016-03-11 01:28:24 +0700

    SSO -- alpha integrate wp logins to Larvale

--- public/ideas/api/index.php
+++ public/ideas/api/index.php
@@ -1,26 +1,33 @@
 <?php
-require('../wp-blog-header.php');
-if($_REQUEST['call'] == 'login'){
-    if($_REQUEST['username'] == '' || $_REQUEST['password'] == ''){
-        echo 'Empty login credentials';
-    }else {
-        $creds = array();
-        $creds['user_login'] = $_REQUEST['username'];
-        $creds['user_password'] = $_REQUEST['password'];
-        $creds['remember'] = true;
-        $user = wp_signon($creds, false);
-        if (is_wp_error($user)) {
-            echo $user->get_error_message();
-        } else {
-            $user_id = $user->data->ID;
-            wp_set_current_user($user_id, $creds['user_login']);
-            wp_set_auth_cookie($user_id);
-            echo 'logged in!';
-        }
-    }
-}elseif($_REQUEST['call'] == 'logout'){
-    wp_logout();
-    echo 'logged out!';
-}
+    require('../wp-blog-header.php');
+    $response = [];
 
+    if($_REQUEST['call'] == 'login'){
+        if($_REQUEST['username'] == '' || $_REQUEST['password'] == ''){
+            $response['error'] = 'Empty login credentials';
+        }else {
+            $creds = array();
+            $creds['user_login'] = $_REQUEST['username'];
+            $creds['user_password'] = $_REQUEST['password'];
+
+            if($_REQUEST['remember'] == 1){
+                $creds['remember'] = true;
+            }
+
+            $user = wp_signon($creds, false);
+            if (is_wp_error($user)) {
+                echo  $response['error'] = $user->get_error_message();
+            } else {
+                $user_id = $user->data->ID;
+                wp_set_current_user($user_id, $creds['user_login']);
+                wp_set_auth_cookie($user_id);
+                $response['success'] = 'Logged in!';
+            }
+        }
+    }elseif($_REQUEST['call'] == 'logout'){
+        wp_logout();
+        $response['success'] = 'Logged out!';
+    }
+
+    echo json_encode($response);
 ?>
