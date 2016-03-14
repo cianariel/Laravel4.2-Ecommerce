4e9af9ae9897f76c1e705e32917ced0f1c5fb46f
Alex Velikanov <biz@alexvelikanov.com>
2016-03-14 23:17:02 +0700

    Remove unused methods

--- app/Models/WpUser.php
+++ app/Models/WpUser.php
@@ -11,41 +11,18 @@
 
         protected $connection = 'wpdb';
         protected $table = 'users';
-
-
-        public static function login($username, $password, $remember){
-
-            if($remember){
-                $remember = 1;
-            }else{
-                $remember = 0;
-            }
-            $url = 'http://ideaing.dev/ideas/api/?call=login&username=' . $username . '&password=' . $password . '&remember=' . $remember;
-
-//            $response = file_get_contents($url);
-
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_HEADER, 0);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_ENCODING, "");
-            $response = curl_exec($ch);
-
-            return json_decode($response);
-        }
-
-        public static function loginAndGoToWP($username, $password, $remember){
-
-            if($remember){
-                $remember = 1;
-            }else{
-                $remember = 0;
-            }
-            $url = 'http://ideaing.dev/ideas/api/?call=login&username=' . $username . '&password=' . $password . '&remember=' . $remember;
-
-            return Redirect::to($url);
-
-//            $response = file_get_contents($url);
+//
+//
+//        public static function login($username, $password, $remember){
+//
+//            if($remember){
+//                $remember = 1;
+//            }else{
+//                $remember = 0;
+//            }
+//            $url = 'http://ideaing.dev/ideas/api/?call=login&username=' . $username . '&password=' . $password . '&remember=' . $remember;
+//
+////            $response = file_get_contents($url);
 //
 //            $ch = curl_init();
 //            curl_setopt($ch, CURLOPT_URL, $url);
@@ -55,13 +32,36 @@
 //            $response = curl_exec($ch);
 //
 //            return json_decode($response);
-        }
-
-        public static function logout(){
-
-            $response = file_get_contents(URL::to('/') . 'ideas/api/?call=logout');
-            return json_decode($response);
-
-        }
-
+//        }
+//
+//        public static function loginAndGoToWP($username, $password, $remember){
+//
+//            if($remember){
+//                $remember = 1;
+//            }else{
+//                $remember = 0;
+//            }
+//            $url = 'http://ideaing.dev/ideas/api/?call=login&username=' . $username . '&password=' . $password . '&remember=' . $remember;
+//
+//            return Redirect::to($url);
+//
+////            $response = file_get_contents($url);
+////
+////            $ch = curl_init();
+////            curl_setopt($ch, CURLOPT_URL, $url);
+////            curl_setopt($ch, CURLOPT_HEADER, 0);
+////            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+////            curl_setopt($ch, CURLOPT_ENCODING, "");
+////            $response = curl_exec($ch);
+////
+////            return json_decode($response);
+//        }
+//
+//        public static function logout(){
+//
+//            $response = file_get_contents(URL::to('/') . 'ideas/api/?call=logout');
+//            return json_decode($response);
+//
+//        }
+//
     }
