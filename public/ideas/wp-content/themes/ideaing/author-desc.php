a6806151862e7028296b185041beb8b0fe9f5a09
Tanvir Anowar <tanvir@carbon51.com>
2016-03-24 01:02:47 +0600

    +

---
+++ public/ideas/wp-content/themes/ideaing/author-desc.php
@@ -0,0 +1,6 @@
+    <h4 >About the Author, <span ng-bind="authorName"></span>  </h4>
+    <div class="col-md-1 col-sm-2 col-xs-3">
+        <img id="currentPhoto" class="profile-photo" width="150px" ng-src="{{ authorImage }}"
+             onerror="this.src='http://s3-us-west-1.amazonaws.com/ideaing-01/thumb-product-568d28a6701c7-no-item.jpg'"
+             width="170">
+    </div>
