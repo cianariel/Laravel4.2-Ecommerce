8371bd90f6f396fea341f7acae4c6ec6d9a5af8a
Tanvir Anowar <tanvir@carbon51.com>
2016-03-23 09:31:40 +0600

    +

--- app/Http/Controllers/HeartController.php
+++ app/Http/Controllers/HeartController.php
@@ -18,208 +18,52 @@ class CommentController extends ApiController
 {
     public function __construct()
     {
-     //   $this->comment = new Comment();
-    //    $this->user = new User();
+        //   $this->comment = new Comment();
+        //    $this->user = new User();
 
         $this->heart = new Heart();
     }
 
     public function addNotification()
     {
-
-    }
-
-    public function addToHeartCounter()
-    {
+        // saving heart information
         $inputData = \Input::all();
 
         $data['UserId'] = $inputData['uid'];
         $data['ItemId'] = $inputData['iid'];
         $data['Section'] = $inputData['section'];
 
-        $this->heart->addHeartCounter($data);
+        $data['Link'] = $inputData['plink'];
 
 
-    }
+        $result = $this->heart->addHeartCounter($data);
+
+        $notification['HeartInfo'] = $this->heart->findHeartCountForItem($data);
+        $notification['SenderId'] = $inputData['uid'];
+        $notification['Permalink'] = $data['Link'];
+
+        $dataStr = date("Y-m-d H:i:s");
+        $notification['PostTime'] = (string)$dataStr;
 
 
-
-    // Broadcast notification to the relevant users
-    public function addProductNotification($data)
-    {
-        $this->addNotification($data, 'product');
-    }
-
-    public function addIdeasNotification($data)
-    {
-        $this->addNotification($data, 'ideas');
-    }
-
-
-    /*public function dt()
-    {
-
-        $commnet = new \App\Models\Comment();
-        return $commnet->ideasCommentCounter(2883);
-    }*/
-
-    /**
-     * @param $data
-     * @param $section
-     * @internal param $info
-     */
-    private function addNotification($data,$section)
-    {
+        // sending notification
         $info['Users'] = [];
-        foreach ($data['CommentInfo'] as $commenter) {
+        foreach ($data['HeartInfo'] as $heart) {
 
-            if ($commenter['UserId'] != $data['SenderId'])
-                array_push($info['Users'], $commenter['UserId']);
+            if ($heart['UserId'] != $data['SenderId'])
+                array_push($info['Users'], $heart['UserId']);
         }
         $info['Users'] = array_unique($info['Users']);
         $info['Category'] = 'comment';//$data['Category'];
         $info['SenderId'] = $data['SenderId'];
-        $info['Permalink'] = $section.'/' . $data['Permalink'] . '#comment';
+        $info['Permalink'] = $data['Section'] . '/' . $data['Permalink'] ;
         $info['PostTime'] = $data['PostTime'];
         $info['ItemTitle'] = $data['ItemTitle'];
-        $info['Section'] = $section;
+        $info['Section'] = $data['Section'];
 
         $this->user->sendNotificationToUsers($info);
-    }
 
-    public function addCommentForProduct()
-    {
-        $inputData = \Input::all();
-
-        if (!empty($inputData['comment']) && !empty($inputData['pid'])) {
-            $data['UserId'] = $inputData['uid'];
-            $data['ProductId'] = $inputData['pid'];
-            $data['Comment'] = $inputData['comment'];
-
-            $data['Link'] = $inputData['plink'];
-            $data['Flag'] = 'Show';
-
-            $result = $this->comment->addCommentForProduct($data);
-
-            $notification['CommentInfo'] = $this->comment->findCommentForProduct(['ProductId' => $inputData['pid']]);
-            $notification['SenderId'] = $inputData['uid'];
-            $notification['Permalink'] = $data['Link'];
-
-            $dataStr = date("Y-m-d H:i:s");
-            $notification['PostTime'] = (string)$dataStr;
-
-            // Add product title in the notification
-            $product = Product::where('id',$inputData['pid'])->first();
-            $notification['ItemTitle'] = $product['product_name'];
-          //  $notification['Section'] = 'product';
-
-
-            $this->addProductNotification($notification);
-
-            return $this->setStatusCode(\Config::get("const.api-status.success"))
-                        ->makeResponse($result);
-
-        }
 
     }
-
-    // add comment for ideas
-    public function addCommentForIdeas()
-    {
-        $inputData = \Input::all();
-
-        if (!empty($inputData['comment']) && !empty($inputData['pid'])) {
-            $data['UserId'] = $inputData['uid'];
-            $data['ItemId'] = $inputData['pid'];
-            $data['Comment'] = $inputData['comment'];
-
-            $data['Link'] = $inputData['plink'];
-            $data['Flag'] = 'Show';
-
-            $result = $this->comment->addCommentForIdeas($data);
-
-            $notification['CommentInfo'] = $this->comment->findCommentForIdeas(['ItemId' => $inputData['pid']]);
-            $notification['SenderId'] = $inputData['uid'];
-            $notification['Permalink'] = $data['Link'];
-
-            // $dateTime = Carbon::now();
-            $dataStr = date("Y-m-d H:i:s");//$dateTime->date;
-            $notification['PostTime'] = (string)$dataStr;//$data['Link'];
-
-            // Add product title in the notification
-            $product = WpPost::where('ID',$inputData['pid'])->first();
-            $notification['ItemTitle'] = $product['post_title'];
-
-            $this->addIdeasNotification($notification);
-
-            return $this->setStatusCode(\Config::get("const.api-status.success"))
-                        ->makeResponse($result);
-
-        }
-
-    }
-
-    public function getCommentForProduct($pid = null)
-    {
-        if (!empty($pid)) {
-            $data['ProductId'] = $pid;
-
-            $result = $this->comment->findCommentForProduct($data);
-
-            return $this->setStatusCode(\Config::get("const.api-status.success"))
-                        ->makeResponse($result);
-
-        }
-
-    }
-
-    public function getCommentForIdeas($pid = null)
-    {
-        if (!empty($pid)) {
-            $data['ItemId'] = $pid;
-
-            $result = $this->comment->findCommentForIdeas($data);
-
-            return $this->setStatusCode(\Config::get("const.api-status.success"))
-                        ->makeResponse($result);
-
-        }
-
-    }
-
-
-    public function updateComment()
-    {
-        $inputData = \Input::all();
-
-        if (!empty($inputData['comment']) && !empty($inputData['cid'])) {
-            $data['Id'] = $inputData['cid'];
-            $data['Comment'] = $inputData['comment'];
-
-
-            $result = $this->comment->updateCommentForProduct($data);
-
-            return $this->setStatusCode(\Config::get("const.api-status.success"))
-                        ->makeResponse($result);
-
-        }
-
-    }
-
-    public function deleteComment()
-    {
-        $inputData = \Input::all();
-
-        if (!empty($inputData['cid'])) {
-            $data['Id'] = $inputData['cid'];
-            $result = $this->comment->deleteCommentForProduct($inputData['cid']);
-
-            return $this->setStatusCode(\Config::get("const.api-status.success"))
-                        ->makeResponse($result);
-
-        }
-
-    }
-
 
 }
--- app/Models/Heart.php
+++ app/Models/Heart.php
@@ -28,14 +28,66 @@ class Heart extends Model
 
     public function addHeartCounter($info)
     {
-        $product = Product::where('id', $info['pid'])->first();
+        if ($info['Section'] == 'product') {
+
+            $item = Product::where('id', $info['ItemId'])
+                           ->with('hearts')
+                           ->first();
+
+        } elseif ($info['Section'] == 'ideas') {
+
+            $item = WpPost::where('ID',$info['ItemId'])
+                          ->with('hearts')
+                          ->first();
+        }
+
+       // $product = Product::where('id', $info['pid'])->first();
 
         $heart = new Heart();
-        $heart->user_id = $info['uid'];
+        $heart->user_id = $info['UserId'];
 
-        $result = $product->hearts()->save($heart);
+        $result = $item->hearts()->save($heart);
 
         return $result;
+    }
+
+    public function findHeartCountForItem($data)
+    {
+        if ($data['Section'] == 'product') {
+
+            $item = Product::where('id', $data['ItemId'])
+                              ->with('hearts')
+                              ->first();
+
+        } elseif ($data['Section'] == 'ideas') {
+
+            $item = WpPost::where('ID',$data['ItemId'])
+                            ->with('hearts')
+                            ->first();
+        }
+
+        $itemsHeartCounts = isset($item->hearts)?$item->hearts:[];
+        $HeartCountCollection = new Collection();
+
+        $user = new User();
+
+        foreach($itemsHeartCounts as $singleComment)
+        {
+            $userInfo = $user->getUserById($singleComment['user_id']);
+
+            $data['HeartId'] = $singleComment['id'];
+            $data['UserId'] = $userInfo['id'];
+            $data['UserName'] = $userInfo['name'];
+            $data['UserEmail'] = $userInfo['email'];
+            $data['Picture'] = $userInfo->medias[0]->media_link;
+            $data['PostTime'] = Carbon::createFromTimestamp(strtotime($singleComment['created_at']))->diffForHumans();
+
+            $HeartCountCollection->push($data);
+
+        }
+
+        return $HeartCountCollection;
+
     }
 
 
