b7819fa23bed9c792a33f5bdd91a41db7cb1b254
cianariel <cianariel72@gmail.com>
2016-03-30 10:25:37 +0200

    Fix 5 issues

--- resources/views/product/product-details.blade.php
+++ resources/views/product/product-details.blade.php
@@ -119,9 +119,8 @@
 
                 <nav class="top-product-controls">
                     <ul>
-                        <li><a href="#" class="get-alerts"><i class="m-icon m-icon--alert"></i>&nbsp; Get alerts</a></li>
                         <li class="">
-                            <a href="#" ng-class="['likes', {active: unHeart==false}]"
+                            <a href="#" ng-class="['likes', {active: unHeart!=false}]"
                                ng-init="heartCounterAction(<?php echo $userData['id'] . "," . $productId . "," . "'product'"?>)"
                                ng-click="heartAction(<?php echo $userData['id'] . "," . $productId . "," ."'$permalink'".",". "'product'"?>)"
                             >
@@ -135,6 +134,7 @@
                             </a>
 
                         </li>
+                        <li><a href="#" class="get-alerts"><i class="m-icon m-icon--alert"></i>&nbsp; Get alerts</a></li>
                         <li><a href="#" data-scrollto="#comments" class="comments"><i class="m-icon m-icon--discuss-products"></i>&nbsp; <?php echo "{{ commentsCount }}" ?></a></li>
                     </ul>
                 </nav>
