e1d5b2600744057a104d7eff08a5aeb61e07f778
Tanvir Anowar <tanvir@carbon51.com>
2016-04-02 01:19:48 +0600

    Recently liked user in Ideas.

--- public/ideas/wp-content/themes/ideaing/heart-user-img.php
+++ public/ideas/wp-content/themes/ideaing/heart-user-img.php
@@ -1,11 +1,6 @@
-<div ng-init="heartUsers()">
-    <li class="hidden-xs"><a class="" href="#">
-            <img id="currentPhoto" class="profile-photo" width="40px" ng-src="{{ authorImage }}"
-                 onerror="this.src='http://s3-us-west-1.amazonaws.com/ideaing-01/thumb-product-568d28a6701c7-no-item.jpg'">
-        </a>
-    </li>
-    <li class="hidden-xs"><a class="" href="#">
-            <img id="currentPhoto" class="author" width="40px" ng-src="{{ authorImage }}"
+<div ng-init="heartUsers('ideas')">
+    <li ng-repeat="user in heartUsersInfo" class="hidden-xs"><a class="" href="#">
+            <img id="currentPhoto" class="profile-photo" width="40px" alt="{{ user.name }}" ng-src="{{ user.medias[0].media_link }}"
                  onerror="this.src='http://s3-us-west-1.amazonaws.com/ideaing-01/thumb-product-568d28a6701c7-no-item.jpg'">
         </a>
     </li>
