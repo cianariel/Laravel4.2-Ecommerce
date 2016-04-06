313f904834bd27ae5eb307babca0e7b36206c491
Alex Velikanov <biz@alexvelikanov.com>
2016-04-07 02:20:33 +0700

    CMS slider -- Adjust views and stlyes

---
+++ resources/views/layouts/parts/hero-slider.blade.php
@@ -0,0 +1,26 @@
+<div class="rsContent" style="padding-top: 50px; width: 140%; margin-left: -20%;">
+
+    <?php
+     if(function_exists('is_single')){
+         $sliderContent = getHeroSliderContent();
+     }
+
+    foreach($sliderContent as $item){ ?>
+        <div class="box-item product-box text-center" style="max-width: 33%;float:left;">
+            <div class="img-holder">
+                <img src="<?php echo $item['image']?>">
+            </div>
+            <div class="box-item__label-idea">
+                <a href="<?php echo $item['url']?>" class="box-item__label"><?php echo $item['title']?></a>
+                <div class="clearfix"></div>
+                <a href="<?php echo $item['url']?>" class="box-item__read-more">Read More</a>
+            </div>
+            <div class="box-item__author">
+                <a href="/user/profile/<?php echo $item['authorlink']?>" class="user-widget">
+                    <img class="user-widget__img" src="<?php echo $item['avator']?>">
+                    <span class="user-widget__name"><?php echo $item['author']?></span>
+                </a>
+            </div>
+        </div>
+    <?php } ?>
+</div>
