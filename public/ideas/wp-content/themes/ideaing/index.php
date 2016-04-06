85c090b60a7c64eb17164f2dfadd5146ea279134
bukorosolena <olenabukoros@gmail.com>
2016-04-06 23:14:47 +0200

    fix the sub menu and forgot password

--- public/ideas/wp-content/themes/ideaing/index.php
+++ public/ideas/wp-content/themes/ideaing/index.php
@@ -4,7 +4,7 @@
 $categories = get_categories();
 ?>
 <nav class="mid-nav" >
-    <div class="container">
+    <div class="container hidden-xs">
         <ul class="wrap col-lg-7">
             @foreach($categories as $cat)
                 @if($cat->category_parent == 0)
@@ -17,7 +17,56 @@ $categories = get_categories();
             @endforeach
         </ul>
     </div>
+    <div class="container mobile-menu visible-xs ">
+        <ul class="wrap col-lg-7">
+            <?php $key=0; ?>
+            @foreach($categories as $cat)
+                @if($cat->category_parent == 0)
+                    <?php
+                        if($key>=3)
+                            break;                    
+                    ?>
+                        
+                    <li class="box-link-ul">
+                        <a href="/ideas/{{$cat->slug}}" class="box-link">
+                            {{$cat->name}}
+                        </a>
+                    </li>
+                    <?php $key++ ?>
+                @endif
+            @endforeach
+        </ul>
+        <a class="right-menu-arrow pull-right" data-toggle="#mobile-ideas-menu" href="#">
+            <i class="m-icon--Header-Dropdown down"></i>
+            <i class="m-icon--footer-up-arrow up"></i>
+        </a>
+
+    </div>
 </nav>
+
+<div id="mobile-ideas-menu" class="mobile-top-menu mobile-mid-menu ">
+    <ul>
+        <?php $key=0; ?>
+        @foreach($categories as $cat)
+            @if($cat->category_parent == 0)
+                <?php
+                    if($key<3){
+                        $key++;
+                        continue;                    
+                    }
+                ?>
+                    
+                <li class="box-link-ul">
+                    <a href="/ideas/{{$cat->slug}}" class="box-link">
+                        {{$cat->name}}
+                    </a>
+                </li>
+                <?php $key++ ?>
+            @endif
+        @endforeach
+    </ul>
+</div>
+
 
 <section id="hero" class="landing-hero">
     <div class="hero-background" style="background-image: url('/assets/images/ideas-hero.jpg')"></div>
