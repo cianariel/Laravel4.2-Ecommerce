657249f2171352a80b6c5cc57a5af1648da0d2f3
Alex Velikanov <biz@alexvelikanov.com>
2016-04-07 20:43:47 +0700

    Hero slider -- beta JS

--- public/ideas/wp-content/themes/ideaing/index.php
+++ public/ideas/wp-content/themes/ideaing/index.php
@@ -27,9 +27,9 @@ $categories = get_categories();
 
 
 <div class="app-wrap" id="pagingApp" ng-app="pagingApp" ng-controller="pagingController" ng-cloak>
-    <nav id="hero-nav" class="col-sm-12">
-
-    </nav>
+<!--    <nav id="hero-nav" class="col-sm-12">-->
+<!---->
+<!--    </nav>-->
     <div class="clearfix"></div>
     <div class="homepage-grid center-block" style="min-height:1000px">
         <div class="loader loader-abs" cg-busy="firstLoad"></div>
--- resources/views/layouts/parts/hero-slider.blade.php
+++ resources/views/layouts/parts/hero-slider.blade.php
@@ -1,22 +1,20 @@
-<div style="background-image: url('https://d234pm57oy3062.cloudfront.net/homehero/11/homehero-image-56f8ffa2244c6-landing-hero-3.jpg'); background-size: cover;background-repeat: no-repeat;position: absolute;width: 100%;height: 100%;">
-    <div class="rsContent" style="padding-top: 50px; width: 140%; margin-left: -20%;">
-
+<div style="background-image: url('https://d234pm57oy3062.cloudfront.net/homehero/11/homehero-image-56f8ffa2244c6-landing-hero-3.jpg'); background-size: cover;background-repeat: no-repeat;position: absolute;width: 100%;height: 100%; padding-top: 80px;" id="hero-slider">
         <?php
          if(function_exists('is_single')){
              $sliderContent = getHeroSliderContent();
          }
 
         foreach($sliderContent as $item){ ?>
-            <div class="box-item product-box text-center" style="max-width: 33%;float:left;">
+            <div class="box-item product-box text-center" style="float:left;">
                 <div class="img-holder">
                     <img src="<?php echo $item['image']?>">
                 </div>
                 <div class="box-item__label-idea">
-                    <a href="<?php echo $item['url']?>" class="box-item__label"><?php echo $item['title']?></a>
+                    <a style="float: none;" href="<?php echo $item['url']?>" class="box-item__label"><?php echo $item['title']?></a>
                     <div class="clearfix"></div>
                     <a href="<?php echo $item['url']?>" class="box-item__read-more">Read More</a>
                 </div>
-                <div class="box-item__author">
+                <div class="box-item__author" style="position: relative; margin: 0 auto; width: 193px;top: -75px;">
                     <a href="/user/profile/<?php echo $item['authorlink']?>" class="user-widget">
                         <img class="user-widget__img" src="<?php echo $item['avator']?>">
                         <span class="user-widget__name"><?php echo $item['author']?></span>
@@ -24,70 +22,44 @@
                 </div>
             </div>
         <?php } ?>
-    </div>
 </div>
     <script>
-//        jQuery(document).ready(function($) {
-//            if (window.innerWidth < 480) {
-//
-//                $('#gallery').royalSlider({
-//                    arrowsNav: true,
-//                    keyboardNavEnabled: true,
-//                    controlsInside: false,
-//                    imageScaleMode: 'fit',
-//                    arrowsNavAutoHide: false,
+        jQuery(document).ready(function($) {
+                $('#hero-slider').royalSlider({
+                            arrowsNav: true,
+                    loop: true,
+                    loopRewind: true,
+                    keyboardNavEnabled: true,
+                    controlsInside: false,
+                    imageScaleMode: 'fill',
+                    arrowsNavAutoHide: false,
+//                        autoScaleSlider: true,
+//                    controlNavigation: 'thumbnails',
 //                    autoScaleSlider: true,
-//                    controlNavigation: 'thumbnails',
-//                    thumbsFitInViewport: false,
-//                    navigateByClick: true,
-//                    startSlideId: 0,
-//                    autoPlay: false,
-//                    transitionType: 'move',
-//                    globalCaption: false,
-//                    deeplinking: {
-//                        enabled: true,
-//                        change: false
-//                    },
-//                    thumbs: {
-//                        appendSpan: true,
-//                        firstMargin: false,
-////                                orientation: 'horizntal',
-//                    },
-//                    loop: true
-////                            imgWidth: 1400,
-////                            imgHeight: 680
-//                });
-//            } else {
-//                $('#gallery').royalSlider({
-////                            arrowsNav: true,
-//                    loop: false,
-//                    keyboardNavEnabled: true,
-//                    controlsInside: false,
-//                    imageScaleMode: 'fit',
-//                    arrowsNavAutoHide: false,
-////                        autoScaleSlider: true,
-//                    controlNavigation: 'thumbnails',
-//                    thumbsFitInViewport: false,
-//                    navigateByClick: true,
-//                    startSlideId: 0,
-//                    autoPlay: false,
-//                    transitionType: 'move',
-//                    globalCaption: false,
-//                    deeplinking: {
-//                        enabled: true,
-//                        change: false
-//                    },
-//                    thumbs: {
-//                        arrows: true,
-//                        appendSpan: true,
-//                        firstMargin: false,
-//                        orientation: 'vertical'
-//                    },
-//                    loop: true
-//
-////                        imgWidth: 1400,
-////                        imgHeight: 680
-//                });
+                    autoScaleSliderWidth: 1480,
+//                    autoScaleSliderHeight: 394,
+                    thumbsFitInViewport: false,
+                    navigateByClick: true,
+                    startSlideId: 0,
+                    autoPlay: false,
+                    transitionType: 'move',
+                    globalCaption: false,
+                    deeplinking: {
+                        enabled: true,
+                        change: false
+                    },
+                  visibleNearby: {
+                    enabled: true,
+//                    centerArea: 0.3,
+                    center: true,
+//                    breakpoint: 650,
+//                    breakpointCenterArea: 0.64,
+                    navigateByCenterClick: true
+                }
+
+//                        imgWidth: 1400,
+//                        imgHeight: 680
+                });
 //            }
-//        });
+        });
     </script>
