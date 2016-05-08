482987d2512c115f2c80f4f37da95ab0433eff5d
Tanvir Anowar <tanvir@carbon51.com>
2016-05-08 19:11:44 +0600

    ++

--- resources/assets/main/js/angular-custom/public.common.js
+++ resources/assets/main/js/angular-custom/public.common.js
@@ -1011,7 +1011,8 @@ publicApp.controller('publicController', ['$rootScope', '$scope', '$http', '$win
             }).success(function (data) {
                 //   console.log(data.data);
 
-                var WpLoginURL = 'https://ideaing.com/ideas/api?call=login&username=' + $scope.Email + '&password=' + $scope.Password + '&remember=' + $scope.rememberMe;
+             //   var WpLoginURL = 'https://ideaing.com/ideas/api?call=login&username=' + $scope.Email + '&password=' + $scope.Password + '&remember=' + $scope.rememberMe;
+                var WpLoginURL = '/ideas/api?call=login&username=' + $scope.Email + '&password=' + $scope.Password + '&remember=' + $scope.rememberMe;
 
                 $http({
                     url: WpLoginURL,
@@ -1020,13 +1021,16 @@ publicApp.controller('publicController', ['$rootScope', '$scope', '$http', '$win
                 }).success(function (data) {
                     var from = $location.search().from; // TODO -- disable this
                     if (from === 'cms') {
-                        window.location = 'https://ideaing.com/ideas/wp-admin';
+                      //  window.location = 'https://ideaing.com/ideas/wp-admin';
+                        window.location = '/ideas/wp-admin';
+
                     }
                 }).error(function (data, status, headers, config) {
                     if (data.success) {
                         var from = $location.search().from; // TODO -- disable this
                         if (from === 'cms') {
-                            window.location = 'https://ideaing.com/ideas/wp-admin';
+                        //    window.location = 'https://ideaing.com/ideas/wp-admin';
+                            window.location = '/ideas/wp-admin';
                         }
                     }
                 });
