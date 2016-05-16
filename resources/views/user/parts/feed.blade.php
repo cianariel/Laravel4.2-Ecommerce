b35af2bcec256d45ec9839263ee21a3cd30514df
Tanvir Anowar <tanvir@carbon51.com>
2016-05-17 01:07:07 +0600

    +++

--- resources/views/user/parts/feed.blade.php
+++ resources/views/user/parts/feed.blade.php
@@ -1,4 +1,4 @@
-<div class="container">
+<div id="loadDynamicData" class="container">
     <div class="col-md-3 side-bar hidden-xs hidden-sm">
         <div class="row">
             <ul class="nav sidenav">
@@ -29,7 +29,66 @@
             </ul>
         </div>
     </div>
-    <div class="col-md-9 main-content" ng-init="userActivityList({{$userData['id']}},null)">
+    {{--<div class="col-md-9 main-content" ng-init="userActivityList({{$userData['id']}},5)">
+
+        @foreach($activity as $item)
+
+            <div class="feed-content row"
+                 @if($item['Type']=='comment') ng-show="showComment" @else ng-show="showHeart" @endif >
+                <div class="feed-header ">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="pull-left">
+                                <img src="{{$profile}}" width="50px" class="profile-photo" alt="">
+                            </div>
+                            <div class="pull-left name-time">
+                                <strong>{{$fullname}}</strong> --}}{{--<i class="m-icon--heart-solid"></i>--}}{{--<br>
+                                <span class="time">{{ $item['UpdateTime'] }}</span>
+                            </div>
+                            <div class="pull-right">
+                                --}}{{--<a href="#">
+                                    Actions
+                                    <span class="caret"></span>
+                                </a>--}}{{--
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="feed-body ">
+                    <div class="row">
+
+                        @if($item['Image'] !='')
+                            <div class="col-xs-4">
+                                <img src="{{ $item['Image'] }}">
+                            </div>
+                        @endif
+
+                        <div @if($item['Type'] =='heart') class="col-xs-12" @else class="col-xs-8" @endif>
+                            <strong>
+                                {{$fullname}} recently @if($item['Type']=='comment') commented @else liked @endif a
+                                @if($item['Section']=='product') product @else idea @endif
+                            </strong>: <a href="{{ $item['Link'] }}" target="_blank">{{ $item['Title'] }}</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="feed-footer ">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="pull-left">
+                                <span class="favorite"><i class="m-icon--heart-solid"></i> {{ $item['HeartCount'] }}</span>
+                                <span class="comment"><i class="m-icon--buble"></i> {{ $item['CommentCount'] }}</span>
+                            </div>
+                            --}}{{--<div class="pull-right">
+                                <a href="{{ $item['Link'] }}" target="_blank"><strong>View original post</strong></a>
+                            </div>--}}{{--
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>--}}
+
+    <div class="col-md-9 main-content" ng-init="userActivityList({{$userData['id']}},5)">
 
         @foreach($activity as $item)
 
@@ -87,4 +146,13 @@
             </div>
         @endforeach
     </div>
+
+
 </div>
+<script>
+    $(window).scroll(function () {
+        if ($(document).height() <= $(window).scrollTop() + $(window).height()) {
+            angular.element(document.getElementById('loadDynamicData')).scope().userActivityList({{$userData['id']}},5);
+        }
+    });
+</script>
