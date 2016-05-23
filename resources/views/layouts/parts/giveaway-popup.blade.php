09a2c8f74d54ddf9b106570c131e3fe776c581d6
Alex Velikanov <biz@alexvelikanov.com>
2016-05-23 20:05:54 +0700

    Add giveaway popup

---
+++ resources/views/layouts/parts/giveaway-popup.blade.php
@@ -0,0 +1,31 @@
+<?php
+    if(!isset($theGiveAway)){
+        if(!function_exists('is_single')){
+            $theGiveAway = PageHelper::getCurrentGiveaway();
+        }else{
+            $json = file_get_contents('http://ideaing.dev/api/giveaway/get-current');
+            $theGiveAway = json_decode($json);
+        }
+    }
+?>
+
+<div id="giveaway-popup" class="col-xs-12 hidden-soft">
+     <span class="close-button close-login" data-toggle="#giveaway-popup">
+        <i class="m-icon--Close"></i>
+    </span>
+
+    <h4>Ideaing Giveaway</h4>
+
+    <a href="/giveaway/<?php echo $theGiveAway->giveaway_permalink ?>">
+        <img src="<?php echo $theGiveAway->giveaway_image ?>" title="<?php echo $theGiveAway->giveaway_image_title ?>" class="col-xs-2 center-block img-responsive" alt="<?php echo $theGiveAway->giveaway_image_alt ?>" />
+    </a>
+
+    <h5>
+        <a class="white" href="/giveaway/<?php echo $theGiveAway->giveaway_permalink ?>">
+            <?php echo $theGiveAway->giveaway_title ?>
+        </a>
+    </h5>
+
+    <a class="btn btn-info col-xs-3 center-block" href="/giveaway/<?php echo $theGiveAway->giveaway_permalink ?>">ENTER</a>
+</div>
+
