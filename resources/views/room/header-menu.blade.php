59222b10af16e86e2956be1c9efb4fc7ac8ebc6e
Alex Velikanov <biz@alexvelikanov.com>
2016-05-25 14:00:57 +0700

    Adjust mobile styling

--- resources/views/room/header-menu.blade.php
+++ resources/views/room/header-menu.blade.php
@@ -48,9 +48,8 @@
     
     <div class="container mobile-menu visible-xs full-sm fixed-sm">
         <ul class="wrap col-lg-9">
-            <li ><a class="box-link @if(!isset($roomInformation['Permalink']) || $roomInformation['Permalink'] == '/') active @endif"   href="/">Smart Home</a></li>
             <li><a class="box-link @if(isset($roomInformation['Permalink']) && $roomInformation['Permalink'] == 'kitchen') active @endif " href="{{url('idea/kitchen')}}">Kitchen</a></li>
-            <li><a class="box-link @if(isset($roomInformation['Permalink']) && $roomInformation['Permalink'] == 'bath') active @endif " href="{{url('idea/bath')}}">Bath</a></li>
+            <li><a class="box-link @if(isset($roomInformation['Permalink']) && $roomInformation['Permalink'] == 'bath') active @endif " href="{{url('idea/lighting')}}">Lighting</a></li>           <li><a class="box-link @if(isset($roomInformation['Permalink']) && $roomInformation['Permalink'] == 'bath') active @endif " href="{{url('idea/security')}}">Security</a></li>
         </ul>
         <a class="right-menu-arrow pull-right" data-toggle="#mobile-home-menu" href="#">
             <i class="m-icon--Header-Dropdown down"></i>
