a8a1ababde8e90db69d4c571b46a1060200b2ce1
Alex Velikanov <biz@alexvelikanov.com>
2016-06-02 13:14:32 +0700

    CMS Single -- Adjust overlays on thumbs

--- public/ideas/wp-content/themes/ideaing/functions.php
+++ public/ideas/wp-content/themes/ideaing/functions.php
@@ -709,5 +709,30 @@ function single_post_tag_classes( $classes ) {
 return $classes;
 }
 
+/**
+ * Disable responsive image support (test!)
+ */
+
+// Clean the up the image from wp_get_attachment_image()
+add_filter( 'wp_get_attachment_image_attributes', function( $attr )
+{
+    if( isset( $attr['sizes'] ) )
+        unset( $attr['sizes'] );
+
+    if( isset( $attr['srcset'] ) )
+        unset( $attr['srcset'] );
+
+    return $attr;
+
+}, PHP_INT_MAX );
+
+// Override the calculated image sizes
+add_filter( 'wp_calculate_image_sizes', '__return_false',  PHP_INT_MAX );
+
+// Override the calculated image sources
+add_filter( 'wp_calculate_image_srcset', '__return_false', PHP_INT_MAX );
+
+// Remove the reponsive stuff from the content
+remove_filter( 'the_content', 'wp_make_content_images_responsive' );
 
 ?>
