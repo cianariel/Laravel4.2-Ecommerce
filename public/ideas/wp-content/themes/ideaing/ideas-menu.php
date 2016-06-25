16542a2ebb144f9442f9de4a3ce9b2668dedeaa7
Alex Velikanov <biz@alexvelikanov.com>
2016-06-25 21:38:47 +0700

    Change WP category names

--- public/ideas/wp-content/themes/ideaing/ideas-menu.php
+++ public/ideas/wp-content/themes/ideaing/ideas-menu.php
@@ -4,10 +4,10 @@ $currentCat = $cat->slug;
 ?>
                 <ul class="wrap col-lg-4 col-lg-6">
                      <li class="box-link-ul {{$currentCat}}">
-                                <a href="/ideas/smart-home" class="box-link <?php echo $currentCat == 'smart-home' ? 'active' : '' ?>">Smart Home</a>
+                                <a href="/ideas/usage-list" class="box-link <?php echo $currentCat == 'usage-list' ? 'active' : '' ?>">Usage</a>
                             </li>
                             <li class="box-link-ul">
-                                <a href="/ideas/reviews" class="box-link <?php echo $currentCat == 'reviews' ? 'active' : '' ?>">Reviews</a>
+                                <a href="/ideas/best-reviews" class="box-link <?php echo $currentCat == 'best-reviews' ? 'active' : '' ?>">Best</a>
                             </li>
                              <li class="box-link-ul">
                                 <a href="/ideas/style" class="box-link <?php echo $currentCat == 'style' ? 'active' : '' ?>">Style</a>
