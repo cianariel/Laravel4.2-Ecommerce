6663c8fef584d0249878bcb72c28c84ae37642af
Tanvir Anowar <tanvir@carbon51.com>
2016-07-01 21:44:55 +0600

    Fixed footer of all email.

---
+++ resources/views/email/email-footer.blade.php
@@ -0,0 +1,96 @@
+<table bgcolor="#ffffff" width="600" cellpadding="0" cellspacing="0" border="0" align="center"
+       class="devicewidth">
+    <tbody>
+    <tr>
+        <td>
+            <!-- start of right column -->
+            <table width="290" align="center" border="0" cellpadding="0" cellspacing="0"
+                   class="devicewidth">
+                <tbody>
+
+                <tr>
+                    <td>
+                        <!-- start of text content table -->
+                        <table width="290" align="left" border="0" cellpadding="0"
+                               cellspacing="0" class="devicewidth">
+                            <tbody>
+                            <!-- image -->
+                            <tr>
+                                <td width="290" height="" align="center" class="devicewidth">
+
+                                    <a href="https://www.facebook.com/ideaingsmarterliving"><img width="24" height="25"
+                                                                                                 src="https://cdn-images.mailchimp.com/icons/social-block-v2/color-facebook-48.png"/></a>
+                                    &nbsp; &nbsp;
+
+                                    <a href="https://twitter.com/ideaing/"><img  width="24" height="25"
+                                                                                 src="https://cdn-images.mailchimp.com/icons/social-block-v2/color-twitter-48.png"/></a>
+                                    &nbsp; &nbsp;
+
+                                    <a href="https://www.pinterest.com/ideaing_com"><img  width="24" height="25"
+                                                                                          src="https://cdn-images.mailchimp.com/icons/social-block-v2/color-pinterest-48.png"/></a>
+                                    &nbsp; &nbsp;
+                                    <a href="https://www.instagram.com/ideaing_com/"><img  width="24" height="25"
+                                                                                           src="https://cdn-images.mailchimp.com/icons/social-block-v2/color-instagram-48.png"/></a>
+                                    &nbsp; &nbsp;
+                                    <a href="http://google.com/+Ideaingsmarterliving"><img  width="24" height="25"
+                                                                                            src="https://cdn-images.mailchimp.com/icons/social-block-v2/color-googleplus-48.png"/></a>
+
+                                </td>
+                            </tr>
+
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+
+                </tbody>
+            </table>
+            <!-- end of right column -->
+        </td>
+    </tr>
+    <tr>
+        <!-- start of text content table -->
+        <table width="600" align="left" border="0" cellpadding="0" height="10px"
+               cellspacing="0" class="devicewidth">
+            <tbody>
+            <!-- image -->
+            <tr>
+                <td width="" height="" align="center" class="devicewidth">
+                    <hr/>
+                </td>
+            </tr>
+            <tr>
+                <td width="" height="" align="center" class="devicewidth"
+                    style="font-family: Helvetica, arial, sans-serif; font-size: 16px; color: #666666; text-align:center; line-height: 30px;"
+                    st-content="fulltext-content">
+                    <br/>
+                    <i>Copyright © 2016 Idea Centric LLC,</i> All rights reserved.<br/>
+                    You are on this list because you are a subscriber of Ideaing.<br/>
+                    <br/>
+                    <strong>Our mailing address is:</strong><br/>
+                    Idea Centric LLC<br/>
+                    Gramercy<br/>
+                    Irvine, Ca 92614<br/>
+                    <br/>
+
+                    <a target="_blank" style="text-decoration:underline ; color: #0a8cce" href="http://ideaing.us12.list-manage.com/vcard?u=96801fe1d5d2163ffc6ff717e&id=eeb6e8c94f">Add us to your address book</a>
+                    <br/>
+                    <br/>
+
+                    Don't want to hear from Ideaing?<br/>
+                    You can <a style="text-decoration: none; color: #0a8cce"
+                               href="https://ideaing.com/unsubscribe">unsubscribe</a> from this list<br/>
+
+                </td>
+            </tr>
+
+
+            </tbody>
+        </table>
+        <!-- end of text content table -->
+    </tr>
+    <!-- Spacing -->
+
+    <!-- Spacing -->
+    </tbody>
+</table>
