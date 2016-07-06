616d5f3e2f7ebd9e63dec5c48a7963a3519214cd
Alex Velikanov <biz@alexvelikanov.com>
2016-07-06 16:06:40 +0700

    Adjust heading

--- resources/views/layouts/parts/share-buttons.blade.php
+++ resources/views/layouts/parts/share-buttons.blade.php
@@ -1,4 +1,4 @@
-        <li class="all-shares"><b class="share-count all"></b><br/>all shares</li>
+        <li class="all-shares"><b class="share-count all"></b><br/>all <span class="hidden-620">shares</span></li>
         <li><a data-service="facebook" class="fb" href="#" ng-click="openSharingModal('facebook')"><i class="m-icon m-icon--facebook-id"></i> <b class="fb share-count"></b></a></li>
         <li><a data-service="twitter"  class="twi" href="#" ng-click="openSharingModal('twitter')"><i class="m-icon  m-icon--twitter-id"></i> <b class="twi share-count"></b></a></li>
         <li><a data-service="googleplus"  class="gp" href="#"  ng-click="openSharingModal('googleplus')"><i class="m-icon m-icon--google-plus-id"></i> <b class="gp share-count"></b></a></li>
@@ -12,4 +12,4 @@
           }
         ?>
 
-        <li><a  class="email hidden" href="mailto:?subject=Check this out: <?php echo $theTitle ?>&amp;body=Check  this out on IDEAING: <?php echo "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]" ?>" href="#" ><i class="m-icon m-icon--email-form-id"></i></a></li>
+        <li class="email-wrap"><a  class="email" href="mailto:?subject=Check this out: <?php echo $theTitle ?>&amp;body=Check  this out on IDEAING: <?php echo "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]" ?>" href="#" ><i class="m-icon m-icon--email-form-id"></i></a></li>
