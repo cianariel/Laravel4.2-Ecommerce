d3d14b343d460c1d5768febbcd9a03da8780b35a
Alex Velikanov <biz@alexvelikanov.com>
2016-07-06 20:38:03 +0700

    ++

--- resources/views/layouts/parts/share-buttons.blade.php
+++ resources/views/layouts/parts/share-buttons.blade.php
@@ -1,4 +1,4 @@
-        <li class="all-shares"><b class="share-count all"></b><br/>all <span class="hidden-620">shares</span></li>
+        <li class="all-shares"><b class="share-count all"></b><br/><span class="hidden-620">all</span> shares</li>
         <li><a data-service="facebook" class="fb" href="#" ng-click="openSharingModal('facebook')"><i class="m-icon m-icon--facebook-id"></i> <b class="fb share-count"></b></a></li>
         <li><a data-service="twitter"  class="twi" href="#" ng-click="openSharingModal('twitter')"><i class="m-icon  m-icon--twitter-id"></i> <b class="twi share-count"></b></a></li>
         <li><a data-service="googleplus"  class="gp" href="#"  ng-click="openSharingModal('googleplus')"><i class="m-icon m-icon--google-plus-id"></i> <b class="gp share-count"></b></a></li>
