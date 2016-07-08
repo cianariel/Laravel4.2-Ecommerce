b103b00e6f6d9d4c3fc42538a611a8c418856a90
Alex Velikanov <biz@alexvelikanov.com>
2016-07-08 15:10:21 +0700

    Adjust mobile room menu

--- resources/views/room/header-menu.blade.php
+++ resources/views/room/header-menu.blade.php
@@ -1,13 +1,29 @@
 <div id="mobile-home-menu" class="mobile-top-menu mobile-mid-menu ">
     <ul>
-        <li><a class="box-link @if(isset($roomInformation['Permalink']) && $roomInformation['Permalink'] == 'bedroom') active @endif " href="{{url('idea/bedroom')}}">Bedroom</a></li>
-        <li><a class="box-link @if(isset($roomInformation['Permalink']) && $roomInformation['Permalink'] == 'office') active @endif " href="{{url('idea/office')}}">Office</a></li>
-        <li><a class="box-link @if(isset($roomInformation['Permalink']) && $roomInformation['Permalink'] == 'living') active @endif " href="{{url('idea/living')}}">Living</a></li>
-        <li><a class="box-link @if(isset($roomInformation['Permalink']) && $roomInformation['Permalink'] == 'outdoor') active @endif " href="{{url('idea/outdoor')}}">Outdoor</a></li>
-        <li><a class="box-link @if(isset($roomInformation['Permalink']) && $roomInformation['Permalink'] == 'lighting') active @endif " href="{{url('idea/lighting')}}">Lighting</a></li>
-        <li><a class="box-link @if(isset($roomInformation['Permalink']) && $roomInformation['Permalink'] == 'security') active @endif" href="{{url('idea/security')}}">Security</a></li>
-        <li><a class="box-link @if(isset($roomInformation['Permalink']) && $roomInformation['Permalink'] == 'decor') active @endif" href="{{url('idea/decor')}}">Decor</a></li>
-        <li><a class="box-link @if(isset($roomInformation['Permalink']) && $roomInformation['Permalink'] == 'garage') active @endif" href="{{url('idea/garage')}}">Garage</a></li>
+        @if(!isset($roomInformation['Permalink']) || $roomInformation['Permalink'] != 'bedroom')
+            <li><a class="box-link" href="{{url('idea/bedroom')}}">Bedroom</a></li>
+        @endif
+        @if(!isset($roomInformation['Permalink']) || $roomInformation['Permalink'] != 'office')
+            <li><a class="box-link" href="{{url('idea/office')}}">Office</a></li>
+        @endif
+        @if(!isset($roomInformation['Permalink']) || $roomInformation['Permalink'] != 'living')
+            <li><a class="box-link" href="{{url('idea/living')}}">Living</a></li>
+        @endif
+        @if(!isset($roomInformation['Permalink']) || $roomInformation['Permalink'] != 'outdoor')
+            <li><a class="box-link" href="{{url('idea/outdoor')}}">Outdoor</a></li>
+        @endif
+        @if(!isset($roomInformation['Permalink']) || $roomInformation['Permalink'] != 'lighting')
+            <li class="hidden-xs"><a class="box-link" href="{{url('idea/lighting')}}">Lighting</a></li>
+        @endif
+        @if(!isset($roomInformation['Permalink']) || $roomInformation['Permalink'] != 'security')
+            <li class="hidden-xs"><a class="box-link" href="{{url('idea/security')}}">Security</a></li>
+        @endif
+        @if(!isset($roomInformation['Permalink']) || $roomInformation['Permalink'] != 'decor')
+            <li><a class="box-link" href="{{url('idea/decor')}}">Decor</a></li>
+        @endif
+        @if(!isset($roomInformation['Permalink']) || $roomInformation['Permalink'] != 'garage')
+            <li><a class="box-link" href="{{url('idea/garage')}}">Garage</a></li>
+        @endif
     </ul>
 </div>
 
