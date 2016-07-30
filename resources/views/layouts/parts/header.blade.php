0fa7584f3656821dc6dd3f1598849010f0997297
Alex Velikanov <biz@alexvelikanov.com>
2016-07-30 23:48:41 +0700

    ++

--- resources/views/layouts/parts/header.blade.php
+++ resources/views/layouts/parts/header.blade.php
@@ -148,6 +148,12 @@ if (!isset($theGiveAway)) {
                                     </a>
                                 </li>
                                 <li>
+                                    <a class="category-link__smart-entertainment m-icon-text-holder" href="/ideas/smart-entertainment">
+                                        <i class="hidden-xs hidden-sm hidden-md m-icon m-icon--video"></i>
+                                        <span class="m-icon-text"><span class="hidden-xs hidden-sm">Smart</span> Entertainment</span>
+                                    </a>
+                                </li>
+                                <li>
                                     <a class="category-link__smart-body m-icon-text-holder" href="/ideas/smart-body">
                                         <i class="hidden-xs hidden-sm hidden-md m-icon m-icon--wearables"></i>
                                         <span class="m-icon-text"><span class="hidden-xs hidden-sm">Smart</span> Body</span>
@@ -157,12 +163,6 @@ if (!isset($theGiveAway)) {
                                     <a class="category-link__smart-travel m-icon-text-holder" href="/ideas/smart-travel">
                                         <i class="hidden-xs hidden-sm hidden-md m-icon m-icon--travel"></i>
                                         <span class="m-icon-text"><span class="hidden-xs hidden-sm">Smart</span> Travel</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a class="category-link__smart-entertainment m-icon-text-holder" href="/ideas/smart-entertainment">
-                                        <i class="hidden-xs hidden-sm hidden-md m-icon m-icon--video"></i>
-                                        <span class="m-icon-text"><span class="hidden-xs hidden-sm">Smart</span> Entertainment</span>
                                     </a>
                                 </li>
                                 <li>
