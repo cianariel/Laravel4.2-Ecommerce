74a8928ea165584edd56c57ca9690a1bbd6cc3a8
Alex Velikanov <biz@alexvelikanov.com>
2016-08-07 02:47:25 +0700

    ++

--- resources/views/grid/product.blade.php
+++ resources/views/grid/product.blade.php
@@ -9,7 +9,7 @@
 
 
         <a ng-if="!item.is_deal" href="/ideas">
-            <span class="round-tag__label text-uppercase in" itemprop="articleSection">In <?php echo '{{item.category_name}}' ?>, Products <i class="m-icon m-icon--shopping-bag-light-green white"></i></span>
+            <span class="round-tag__label in" itemprop="articleSection">In <?php echo '{{item.category_name}}' ?>, Products <i class="m-icon m-icon--shopping-bag-light-green white"></i></span>
         </a>
 
         <!--  <a  ng-if="item.is_deal" href="/ideas" class="round-tag round-tag--idea deal">
@@ -17,18 +17,6 @@
               <span class="round-tag__label" itemprop="articleSection">Deal</span>
           </a> -->
 
-        <div class="merchant-widget" itemprop="offers" itemscope itemtype="http://schema.org/Offer">
-            <span class="merchant-widget__price" >
-                <span itemprop="priceCurrency" content="USD">$</span>
-                 <span itemprop="price" content=" <?php echo '{{item.sale_price}}' ?>">
-                     <?php echo '{{item.sale_price}}' ?>
-                 </span>
-            </span>
-            <span class="white">from</span>
-            <img class="merchant-widget__store" alt="<?php echo '{{ item.storeInfo.Description }}' ?>"
-                 ng-src='<?php echo '{{ item.storeInfo.ImagePath }}' ?>'/>
-
-        </div>
 
     </div>
 </div>
