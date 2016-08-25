2046f50a5c0bd34cf84ffc7ebd55198726b308be
Tanvir Anowar <tanvir@carbon51.com>
2016-08-25 10:12:06 +0600

    Email content change.

--- resources/views/email/subscription.blade.php
+++ resources/views/email/subscription.blade.php
@@ -1,161 +1,161 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-   <head>
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      <title>Welcome to Ideaing Exclusive Email</title>
-      <style type="text/css">
-         /* Client-specific Styles */
-         #outlook a {padding:0;} /* Force Outlook to provide a "view in browser" menu link. */
-         body{width:100% !important; -webkit-text-size-adjust:100%; -ms-text-size-adjust:100%; margin:0; padding:0;}
-         /* Prevent Webkit and Windows Mobile platforms from changing default font sizes, while not breaking desktop design. */
-         .ExternalClass {width:100%;} /* Force Hotmail to display emails at full width */
-         .ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {line-height: 100%;} /* Force Hotmail to display normal line spacing.*/
-         #backgroundTable {margin:0; padding:0; width:100% !important; line-height: 100% !important;}
-         img {outline:none; text-decoration:none;border:none; -ms-interpolation-mode: bicubic;}
-         a img {border:none;}
-         .image_fix {display:block;}
-         p {margin: 0px 0px !important;}
-         table td {border-collapse: collapse;}
-         table { border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt; }
-         a {color: #0a8cce;text-decoration: none;text-decoration:none!important;}
-         /*STYLES*/
-         table[class=full] { width: 100%; clear: both; }
-         /*IPAD STYLES*/
-         @media only screen and (max-width: 640px) {
-         a[href^="tel"], a[href^="sms"] {
-         text-decoration: none;
-         color: #0a8cce; /* or whatever your want */
-         pointer-events: none;
-         cursor: default;
-         }
-         .mobile_link a[href^="tel"], .mobile_link a[href^="sms"] {
-         text-decoration: default;
-         color: #0a8cce !important;
-         pointer-events: auto;
-         cursor: default;
-         }
-         table[class=devicewidth] {width: 440px!important;text-align:center!important;}
-         table[class=devicewidthinner] {width: 420px!important;text-align:center!important;}
-         img[class=banner] {width: 440px!important;height:220px!important;}
-         img[class=colimg2] {width: 440px!important;height:220px!important;}
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Welcome to Ideaing Exclusive Email</title>
+    <style type="text/css">
+        /* Client-specific Styles */
+        #outlook a {padding:0;} /* Force Outlook to provide a "view in browser" menu link. */
+        body{width:100% !important; -webkit-text-size-adjust:100%; -ms-text-size-adjust:100%; margin:0; padding:0;}
+        /* Prevent Webkit and Windows Mobile platforms from changing default font sizes, while not breaking desktop design. */
+        .ExternalClass {width:100%;} /* Force Hotmail to display emails at full width */
+        .ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {line-height: 100%;} /* Force Hotmail to display normal line spacing.*/
+        #backgroundTable {margin:0; padding:0; width:100% !important; line-height: 100% !important;}
+        img {outline:none; text-decoration:none;border:none; -ms-interpolation-mode: bicubic;}
+        a img {border:none;}
+        .image_fix {display:block;}
+        p {margin: 0px 0px !important;}
+        table td {border-collapse: collapse;}
+        table { border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt; }
+        a {color: #0a8cce;text-decoration: none;text-decoration:none!important;}
+        /*STYLES*/
+        table[class=full] { width: 100%; clear: both; }
+        /*IPAD STYLES*/
+        @media only screen and (max-width: 640px) {
+            a[href^="tel"], a[href^="sms"] {
+                text-decoration: none;
+                color: #0a8cce; /* or whatever your want */
+                pointer-events: none;
+                cursor: default;
+            }
+            .mobile_link a[href^="tel"], .mobile_link a[href^="sms"] {
+                text-decoration: default;
+                color: #0a8cce !important;
+                pointer-events: auto;
+                cursor: default;
+            }
+            table[class=devicewidth] {width: 440px!important;text-align:center!important;}
+            table[class=devicewidthinner] {width: 420px!important;text-align:center!important;}
+            img[class=banner] {width: 440px!important;height:220px!important;}
+            img[class=colimg2] {width: 440px!important;height:220px!important;}
 
 
-         }
-         .main-text p{
-             padding-bottom: 8px;
-         }
+        }
+        .main-text p{
+            padding-bottom: 8px;
+        }
 
-         /*IPHONE STYLES*/
-         @media only screen and (max-width: 480px) {
-         a[href^="tel"], a[href^="sms"] {
-         text-decoration: none;
-         color: #0a8cce; /* or whatever your want */
-         pointer-events: none;
-         cursor: default;
-         }
-         .mobile_link a[href^="tel"], .mobile_link a[href^="sms"] {
-         text-decoration: default;
-         color: #0a8cce !important; 
-         pointer-events: auto;
-         cursor: default;
-         }
-         table[class=devicewidth] {width: 280px!important;text-align:center!important;}
-         table[class=devicewidthinner] {width: 260px!important;text-align:center!important;}
-         img[class=banner] {width: 280px!important;height:140px!important;}
-         img[class=colimg2] {width: 280px!important;height:140px!important;}
-         td[class=mobile-hide]{display:none!important;}
-         td[class="padding-bottom25"]{padding-bottom:25px!important;}
+        /*IPHONE STYLES*/
+        @media only screen and (max-width: 480px) {
+            a[href^="tel"], a[href^="sms"] {
+                text-decoration: none;
+                color: #0a8cce; /* or whatever your want */
+                pointer-events: none;
+                cursor: default;
+            }
+            .mobile_link a[href^="tel"], .mobile_link a[href^="sms"] {
+                text-decoration: default;
+                color: #0a8cce !important;
+                pointer-events: auto;
+                cursor: default;
+            }
+            table[class=devicewidth] {width: 280px!important;text-align:center!important;}
+            table[class=devicewidthinner] {width: 260px!important;text-align:center!important;}
+            img[class=banner] {width: 280px!important;height:140px!important;}
+            img[class=colimg2] {width: 280px!important;height:140px!important;}
+            td[class=mobile-hide]{display:none!important;}
+            td[class="padding-bottom25"]{padding-bottom:25px!important;}
 
-         }
-         a.fb {
-        display: inline-block;
-        height: 35px;
-        font-size: 1rem;
-        border-radius: 30px;
-        padding: 0 10px 0 0 !important;
-        color: #5C7CBD;
-        text-align: center;
-    }
-    a.insta,a.gplus,a.pint{
-        display: inline-block;
-        height: 35px;
-        font-size: 1rem;
-        border-radius: 30px;
-        padding: 0 10px 0 0 !important;
-        color: #5C7CBD;
-        text-align: center;
-    }
+        }
+        a.fb {
+            display: inline-block;
+            height: 35px;
+            font-size: 1rem;
+            border-radius: 30px;
+            padding: 0 10px 0 0 !important;
+            color: #5C7CBD;
+            text-align: center;
+        }
+        a.insta,a.gplus,a.pint{
+            display: inline-block;
+            height: 35px;
+            font-size: 1rem;
+            border-radius: 30px;
+            padding: 0 10px 0 0 !important;
+            color: #5C7CBD;
+            text-align: center;
+        }
 
-    a.fb i.m-icon {
-      color: white;
-      background: #5C7CBD;
-      padding: 5px;
-      border-radius: 20px;
-      padding-top: 7px;
-      padding-left: 7px;
-      padding-right: 7px;
-      padding-bottom: 6px;
-      float: left; }
-   
-  a.twi {
-    display: inline-block;
-    height: 35px;
-    font-size: 1rem;
-    border-radius: 30px;
-    padding: 0 10px 0 0 !important;
-    color: #079DD1;
-    text-align: center;}
-    a.twi i.m-icon {
-      color: white;
-      background: #079DD1;
-      padding: 5px;
-      border-radius: 20px;
-      padding-top: 7px;
-      padding-left: 7px;
-      padding-right: 7px;
-      padding-bottom: 6px;
-      float: left; }
+        a.fb i.m-icon {
+            color: white;
+            background: #5C7CBD;
+            padding: 5px;
+            border-radius: 20px;
+            padding-top: 7px;
+            padding-left: 7px;
+            padding-right: 7px;
+            padding-bottom: 6px;
+            float: left; }
 
-a.likes {
-    display: inline-block;
-    height: 35px;
-    font-size: 1rem;
-    border-radius: 30px;
-    padding: 0 10px 0 0 !important;
-    color: #fa0033;
-    text-align: center;}
-    a.likes i.m-icon {
-      color: white;
-      background: #fa0033;
-      padding: 5px;
-      border-radius: 20px;
-      padding-top: 7px;
-      padding-left: 7px;
-      padding-right: 7px;
-      padding-bottom: 6px;
-      float: left; }
-      a.discuss {
-    display: inline-block;
-    height: 35px;
-    font-size: 1rem;
-    border-radius: 30px;
-    padding: 0 10px 0 0 !important;
-    color: #dfdfdf;
-    text-align: center;}
-    a.discuss i.m-icon {
-      color: white;
-      background: #dfdfdf;
-      padding: 5px;
-      border-radius: 20px;
-      padding-top: 7px;
-      padding-left: 7px;
-      padding-right: 7px;
-      padding-bottom: 6px;
-      float: left; }
-      </style>
-   </head>
-   <body bgcolor="#ffffff" style="background-color:#ffffff;">
+        a.twi {
+            display: inline-block;
+            height: 35px;
+            font-size: 1rem;
+            border-radius: 30px;
+            padding: 0 10px 0 0 !important;
+            color: #079DD1;
+            text-align: center;}
+        a.twi i.m-icon {
+            color: white;
+            background: #079DD1;
+            padding: 5px;
+            border-radius: 20px;
+            padding-top: 7px;
+            padding-left: 7px;
+            padding-right: 7px;
+            padding-bottom: 6px;
+            float: left; }
+
+        a.likes {
+            display: inline-block;
+            height: 35px;
+            font-size: 1rem;
+            border-radius: 30px;
+            padding: 0 10px 0 0 !important;
+            color: #fa0033;
+            text-align: center;}
+        a.likes i.m-icon {
+            color: white;
+            background: #fa0033;
+            padding: 5px;
+            border-radius: 20px;
+            padding-top: 7px;
+            padding-left: 7px;
+            padding-right: 7px;
+            padding-bottom: 6px;
+            float: left; }
+        a.discuss {
+            display: inline-block;
+            height: 35px;
+            font-size: 1rem;
+            border-radius: 30px;
+            padding: 0 10px 0 0 !important;
+            color: #dfdfdf;
+            text-align: center;}
+        a.discuss i.m-icon {
+            color: white;
+            background: #dfdfdf;
+            padding: 5px;
+            border-radius: 20px;
+            padding-top: 7px;
+            padding-left: 7px;
+            padding-right: 7px;
+            padding-bottom: 6px;
+            float: left; }
+    </style>
+</head>
+<body bgcolor="#ffffff" style="background-color:#ffffff;">
 
 <table id="backgroundTable" st-sortable="header" bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0"
        width="100%">
@@ -243,14 +243,24 @@ a.likes {
                                         <!-- Title -->
                                         <tr class="main-text">
                                             <td style="font-family: Helvetica, arial, sans-serif; font-size: 14pt; color: #333333; text-align:left; line-height: 1.5;">
-                                                <p align="left">Hi! <br>Thank you for subscribing to Ideaing’s weekly newsletter. You’ll receive exclusive tips and deals on home automation to transform your ordinary home to a Smart Home.</p>
+                                                <p align="left">Hi! <br>Thank you for subscribing to our awesome newsletter. You just made the best decision.
+                                                    I’m going to send you weekly tips, hand-picked smart gadgets and exclusive deals to help you live smarter.</p>
+                                                <br/>
+                                                {{--
+                                                <p align="left">You’re just one step away from being part of the Ideaing Family.
+                                                Please follow the link to <a href="{{ url('/').'/signup/'.$email}}" style="color:rgb(250,0,51)">sign-up</a>
+                                                to become a registered user of Ideaing to unlock all features including exclusive giveaways, freebies, and so much more.</p>
+                                                --}}
 
-                                                <p align="left">You’re just one step away from being part of the Ideaing Family. Please follow the link to <a href="{{ url('/').'/signup/'.$email}}" style="color:rgb(250,0,51)">sign-up</a> to become a registered user of Ideaing to unlock all features including exclusive giveaways, freebies, and so much more.</p>
+                                                <p align="left">We’re really passionate about smarter living: smart home, smart body, smart entertainment and smart travel.
+                                                    You’re just one step-away from being part of our Ideaing Family. Please follow the link to sign-up to become a registered
+                                                    user of Ideaing to unlock all the website features including exclusive giveaways on free smart gadgets, rare coupons and so much more.</p>
 
-<p>Looking forward to hearing from you!</p>
-<br/>
-    <p><img style="border-radius: 50%;" class="profile-photo" width="120px" src="https://s3-us-west-1.amazonaws.com/ideaing-01/120-product-56f6a374d0d50-2474a18.jpg"><br/><a href="https://ideaing.com/user/profile/9" style="color:rgb(250,0,51)">Nicole</a><br/>
-Ideaing Team</p>
+                                                <br/>
+                                                <p>Looking forward to hearing from you!</p>
+                                                <br/>
+                                                <p><img style="border-radius: 50%;" class="profile-photo" width="120px" src="https://s3-us-west-1.amazonaws.com/ideaing-01/120-product-56f6a374d0d50-2474a18.jpg"><br/><a href="https://ideaing.com/user/profile/9" style="color:rgb(250,0,51)">Nicole</a><br/>
+                                                    Ideaing Team</p>
                                             </td>
                                         </tr>
                                         <!-- End of Title --><!-- spacing -->
