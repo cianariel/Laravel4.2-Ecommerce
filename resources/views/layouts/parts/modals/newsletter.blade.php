4b5464e4b6637d1f98537fcd536f7bfdf778a671
Alex Velikanov <biz@alexvelikanov.com>
2016-08-31 23:43:05 +0400

    ++

--- resources/views/layouts/parts/modals/newsletter.blade.php
+++ resources/views/layouts/parts/modals/newsletter.blade.php
@@ -1,7 +1,7 @@
 <script type="text/ng-template" id="subscribe_email_popup.html">
     <div id="subscribe_email_popup" class="ns-effect-genie ns-hide relative">
         <div id="publicApp">
-            <div class="col-sm-6 col-xs-12 img-holder no-padding">
+            <div class="col-sm-6 col-xs-12 hidden-xs img-holder no-padding">
                 <h4 class="white relative"><span>Subscribe to the most unique community centered on Living Smarter</span></h4>
                 <div class="seen-on col-xs-12 absolute">
                     <span class="caption"><b class="white">As seen on:</b></span>
@@ -23,12 +23,33 @@
                 </ul>
                 <strong class="red"><?php echo '{{ responseMessage }}' ?></strong>
                 <div class="content-register">
-                         <span class="email-input-holder ">
+
+                    <span class="password-input-holder bordering">
+                        <label class="input__label input__label--madoka">
+                            <svg class="graphic graphic--madoka" width="100%" height="100%" viewBox="0 0 404 77" preserveAspectRatio="none">
+                                <path d="m0,0l404,0l0,77l-404,0l0,-77z"></path>
+                            </svg>
+                            <input class="form-control not-rounded input__field--madoka" placeholder="Password" type="text">
+                        </label>
+
+                        <label class="input__label input__label--madoka">
+                            <svg class="graphic graphic--madoka" width="100%" height="100%" viewBox="0 0 404 77" preserveAspectRatio="none">
+                                <path d="m0,0l404,0l0,77l-404,0l0,-77z"></path>
+                            </svg>
+                             <input class="form-control not-rounded pull-right input__field--madoka" placeholder="Confirm" type="text">
+                        </label>
+                    </span>
+
+                    <span class="email-input-holder bordering">
                                 <i class="m-icon m-icon--email-form-id black"></i>
-                               <input class="form-control not-rounded" ng-model="data.SubscriberEmail" placeholder="me@email.com"
-                                      type="text">
+                                <label class="input__label input__label--madoka">
+                                    <svg class="graphic graphic--madoka" width="100%" height="100%" viewBox="0 0 404 77" preserveAspectRatio="none">
+                                    <path d="m0,0l404,0l0,77l-404,0l0,-77z"></path>
+                                    </svg>
+                                   <input class="form-control not-rounded input__field--madoka" ng-model="data.SubscriberEmail" placeholder="me@email.com" type="text">
+                               </label>
                         </span>
-                    <a class="btn btn-success form-control not-rounded" ng-click="subscribe(data,'popup')">Join and Create a Free account</a>
+                    <a class="btn btn-success form-control not-rounded" ng-click="registerSubscribedUser()">Join and Create a Free account</a>
                 </div>
                 <div class="content-subscribe hidden-soft">
                     <strong class="red"><?php echo '{{ responseMessage }}' ?></strong>
@@ -37,7 +58,7 @@
                                <input class="form-control not-rounded" placeholder="me@email.com"
                                       type="text">
                         </span>
-                    <a class="btn btn-success form-control not-rounded" ng-click="registerSubscribedUser()">Join</a>
+                    <a class="btn btn-success form-control not-rounded"  ng-click="subscribe(data,'popup')">Join</a>
                 </div>
             </div>
             <footer class="black-footer relative black-bg full-wide text-right white overhide"><b   ng-click="hideAndForget()">Maybe Later <i class="m-icon--Close white"></i></b></footer>
