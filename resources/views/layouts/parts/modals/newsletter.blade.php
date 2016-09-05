f9dfc551a164241400b5845d01180ff337e2b10e
Alex Velikanov <biz@alexvelikanov.com>
2016-09-05 15:02:44 +0400

    Newsletter modal tweaks

--- resources/views/layouts/parts/modals/newsletter.blade.php
+++ resources/views/layouts/parts/modals/newsletter.blade.php
@@ -13,7 +13,7 @@
             </div>
             <div class="col-sm-6 col-xs-12 content-holder pale-grey-bg">
                 <div class="col-xs-12 toggles center-block hidden-620">
-                    <div class="swing-lined col-xs-6 no-padding active grey" data-toggle=".content-register" data-hide=".content-subscribe"><span><b>Register</b></span></div>
+                    <div class="swing-lined col-xs-6 no-padding active grey" data-toggle=".content-register" data-hide=".content-subscribe"><span><b>Free account</b></span></div>
                     <div class="swing-lined col-xs-6 no-padding pull-right grey"  data-toggle=".content-subscribe" data-hide=".content-register"><span><b>Email Only</b></span></div>
                 </div>
                 <div class="col-xs-12 toggles center-block hidden-soft shown-620">
