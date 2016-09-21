785b28c70b35eab4a0b73d936e5aade3d7694641
Robert Vanya <rotberup7@gmail.com>
2016-09-22 02:53:43 +0300

    add s3upload plugin, fix issue in gulp, scss

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,5 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass('./resources/assets/main/sass/app.scss', './resources/assets/main/css/app.css');
 });
