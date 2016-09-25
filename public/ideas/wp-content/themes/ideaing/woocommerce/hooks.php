bdc8379f0b1bbe153147a8c827dbdcb21013e0fe
cianariel <cianariel72@gmail.com>
2016-09-25 20:09:29 +0400

    WC cart conditional filters

--- public/ideas/wp-content/themes/ideaing/woocommerce/hooks.php
+++ public/ideas/wp-content/themes/ideaing/woocommerce/hooks.php
@@ -27,7 +27,7 @@ function ideaing_body_class_filter($classes){
   if ( $is_woocommmerce_checkout ){
     $classes[] = $is_woocommmerce_checkout;
   }
-  
+
   return $classes;
 }
 add_filter('body_class', 'ideaing_body_class_filter', 99, 1 );
@@ -46,3 +46,27 @@ function is_ideaing_woocommerce_checkout_page( $default = false, $strict = true 
   ? $strict : $default;
 }
 add_filter('is_ideaing_woocommerce_checkout_page', 'is_ideaing_woocommerce_checkout_page', 10, 2);
+
+/**
+ * If cart is not empty
+ *
+ * @return return boolean
+ * @since WooCommerce Integration 1.0
+ */
+function is_ideaing_cart_empty(){
+
+  return WC()->cart->is_empty();
+}
+add_filter('is_ideaing_cart_empty', 'is_ideaing_cart_empty');
+
+/**
+ * Get cart contents count
+ *
+ * @return return int
+ * @since WooCommerce Integration 1.0
+ */
+function get_ideaing_cart_contents_count(){
+
+  return WC()->cart->get_cart_contents_count();
+}
+add_filter('get_ideaing_cart_contents_count', 'get_ideaing_cart_contents_count');
