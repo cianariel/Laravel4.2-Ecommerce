7713a32df7a8c524183463e4fd63ef46bb33c6bd
Alex Velikanov <biz@alexvelikanov.com>
2016-09-29 00:15:35 +0400

    ++

--- public/ideas/wp-content/themes/ideaing/single.php
+++ public/ideas/wp-content/themes/ideaing/single.php
@@ -323,10 +323,15 @@
 
     <script>
         $(document).ready(function(){ // add Get It Button overlay on images that link to vendors
+            console.log('test me')
+
             $('.article-content').find('img').each(function(){
+                console.log('what what what what')
+
                 if(!$(this).parents('.get-it-inner').length){
                     var theLinkNode = $(this).parent('a');
                     theLinkNode.attr('target', '_blank').wrap('<div class="get-it-inner"></div>');
+                    console.log('buggaga')
                     var strong = theLinkNode.parents('.thumb-box').find('strong a');
                     var text = strong.text();
 
