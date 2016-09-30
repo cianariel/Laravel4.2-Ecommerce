9b56504da24f7b4f14e5b0efcd7c886a4e0236c5
cianariel <cianariel72@gmail.com>
2016-10-01 01:54:20 +0400

    Shipping can go through billing add too

--- public/ideas/wp-content/themes/ideaing/woocommerce/checkout/form-shipping.php
+++ public/ideas/wp-content/themes/ideaing/woocommerce/checkout/form-shipping.php
@@ -62,12 +62,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	<?php if ( apply_filters( 'woocommerce_enable_order_notes_field', get_option( 'woocommerce_enable_order_comments', 'yes' ) === 'yes' ) ) : ?>
 
-		<?php if ( ! WC()->cart->needs_shipping() || wc_ship_to_billing_address_only() ) : ?>
-
-			<h3><?php _e( 'Additional Information', 'woocommerce' ); ?></h3>
-
-		<?php endif; ?>
-
 		<?php foreach ( $checkout->checkout_fields['order'] as $key => $field ) : ?>
 
 			<?php woocommerce_form_field( $key, $field, $checkout->get_value( $key ) ); ?>
