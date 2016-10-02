12a7b056a08c5537324dec941685b66d0f48ebdb
cianariel <cianariel72@gmail.com>
2016-10-02 22:50:48 +0400

    Global cart summary

--- resources/views/layouts/parts/head.blade.php
+++ resources/views/layouts/parts/head.blade.php
@@ -45,6 +45,7 @@ if (screen.width < 992 && screen.width > 620) {
 
 <link rel="stylesheet" href="/assets/fonts/ideaing/style.css">
 <link rel="stylesheet" href="/assets/css/main.css">
+<link rel="stylesheet" href="/assets/css/woocommerce.css">
 
 <?php
   if(!function_exists('is_single') && @$canonicURL){
@@ -53,6 +54,7 @@ if (screen.width < 992 && screen.width > 620) {
 ?>
 
 <script type="text/javascript" src="/assets/js/main.js"></script>
+<script type="text/javascript" src="/assets/js/cart-summary.js"></script>
 <meta name="google-site-verification" content="xiWn24mA3aZopoTkElR97n-HdvsfctffW493Q6x_vZU" />
 
 <script>
