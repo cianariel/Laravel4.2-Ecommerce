b62f7a6be6aa9a0b060452215d1ac44b4b19eaa6
Tanvir Anowar <tanvir@carbon51.com>
2016-10-03 23:01:36 +0600

    Fixed social sharing text.

--- resources/views/user/user-profile.blade.php
+++ resources/views/user/user-profile.blade.php
@@ -69,7 +69,7 @@
                                socialshare-via="{{env('FB_APP')}}"
                                socialshare-type="feed"
                                socialshare-provider="facebook"
-                               socialshare-text="Welcome to Ideaing"
+                               socialshare-text="Join Ideaing"
                                socialshare-hashtags="Ideaing"
                                socialshare-url="https://ideaing.com"
                             >
@@ -81,7 +81,7 @@
                                socialshare-via="{{env('FB_APP')}}"
                                socialshare-type="feed"
                                socialshare-provider="twitter"
-                               socialshare-text="Welcome to Ideaing"
+                               socialshare-text="Join Ideaing"
                                socialshare-hashtags="Ideaing"
                                socialshare-url="https://ideaing.com"
                             >
