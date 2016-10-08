4b8cdabc021d12efb9c5699fde402531d7982eff
cianariel <cianariel72@gmail.com>
2016-10-08 21:30:40 +0400

    Float bag icon

--- resources/views/layouts/parts/footer.blade.php
+++ resources/views/layouts/parts/footer.blade.php
@@ -26,10 +26,10 @@
                     }
 
                     $json = file_get_contents('https://ideaing.com/api/giveaway/get-current/' . $noPopup);
-        
+
                         $theGiveAway = json_decode($json);
                     }
-                } 
+                }
 
             if($theGiveAway && @$theGiveAway->giveaway_permalink){ ?>
                 <a href="/giveaway/<?php echo $theGiveAway->giveaway_permalink ?>">
@@ -72,6 +72,7 @@
 
 <div class="bottom-block hidden-soft">
     <button class="btn btn-success" id="about-button" data-toggle=".about-footer">About</button>
+    <button class="btn btn-success ics--open" id="float-bag"><i class="m-icon--shopping-bag-light-green"></i></button>
     <a href="#" id="back-to-top">
         <i class="m-icon--footer-up-arrow"></i>
     </a>
@@ -110,7 +111,7 @@
     // Track when a registration form is completed (ex. complete subscription, sign up for a service)
     fbq('track', 'CompleteRegistration');
 </script>
- 
+
 
 <?php if(function_exists('is_single') || (Request::segment(1) != 'login' && Request::segment(1) != 'signup' && Request::segment(1) != 'product')){ ?>
      <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
@@ -160,5 +161,3 @@ function slideMe(){
 //    //console.log('ututututut')
 //})
 </script>
-
-
